c2d926fa493b2376689b2575955805cf66ef4d89
Kenneth Moreland <kmorel@sandia.gov>
2014-03-31 15:28:09 -0600

    Add tag-based type list templates
    Provies a list of types in a template like boost::mpl::vector and a
    method to call a functor on each type. However, rather than explicitly
    list each type, uses tags to identify the list. This provides the
    following main advantages:
    
    1. Can use these type lists without creating horrendously long class
    names based on them, making compiler errors easier to read. For example,
    you would have a typename like MyClass<TypeListTagVectors> instead of
    MyClass<TypeList<Id3,Vector2,Vector3,Vector4> > (or worse if variadic
    templates are not supported). This is the main motivation for this
    implementation.
    
    2. Do not require variadic templates and usually few constructions. That
    should speed compile times.
    
    There is one main disadvantage to this approach: It is difficult to get
    a printed list of items in a list during an error. If necessary, it
    probably would not be too hard to make a template to convert a tag to a
    boost mpl vector.

---
+++ vtkm/TypeListTag.h
@@ -0,0 +1,64 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2014 Sandia Corporation.
+//  Copyright 2014 UT-Battelle, LLC.
+//  Copyright 2014. Los Alamos National Security
+//
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
+//============================================================================
+#ifndef vtk_m_TypeListTag_h
+#define vtk_m_TypeListTag_h
+
+#ifndef VTKM_DEFAULT_TYPE_LIST_TAG
+#define VTKM_DEFAULT_TYPE_LIST_TAG ::vtkm::TypeListTagCommon;
+#endif
+
+#include <vtkm/ListTag.h>
+#include <vtkm/Types.h>
+
+namespace vtkm {
+
+struct TypeListTagId : vtkm::ListTagBase<vtkm::Id> { };
+struct TypeListTagId2 : vtkm::ListTagBase<vtkm::Id2> { };
+struct TypeListTagId3 : vtkm::ListTagBase<vtkm::Id3> { };
+struct TypeListTagScalar : vtkm::ListTagBase<vtkm::Scalar> { };
+struct TypeListTagVector2 : vtkm::ListTagBase<vtkm::Vector2> { };
+struct TypeListTagVector3 : vtkm::ListTagBase<vtkm::Vector3> { };
+struct TypeListTagVector4 : vtkm::ListTagBase<vtkm::Vector4> { };
+
+struct TypeListTagIndex
+    : vtkm::ListTagBase3<vtkm::Id,vtkm::Id2,vtkm::Id3> { };
+
+struct TypeListTagReal
+    : vtkm::ListTagBase4<vtkm::Scalar,vtkm::Vector2,vtkm::Vector3,vtkm::Vector4>
+{ };
+
+/// A list of all basic types listed in vtkm/Types.h. Does not include all
+/// possible VTK-m types like arbitrarily typed and sized tuples or math
+/// types like matrices.
+///
+struct TypeListTagAll
+    : vtkm::ListTagJoin<vtkm::TypeListTagIndex, vtkm::TypeListTagReal>
+{ };
+
+/// A list of the most commonly used types across multiple domains. Includes
+/// Id, Scalar, and Vector3.
+///
+struct TypeListTagCommon
+    : vtkm::ListTagBase3<vtkm::Id, vtkm::Scalar, vtkm::Vector3>
+{ };
+
+} // namespace vtkm
+
+#endif //vtk_m_TypeListTag_h
---
+++ vtkm/testing/UnitTestTypeListTag.cxx
@@ -0,0 +1,132 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2014 Sandia Corporation.
+//  Copyright 2014 UT-Battelle, LLC.
+//  Copyright 2014. Los Alamos National Security
+//
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
+//============================================================================
+
+#include <vtkm/TypeListTag.h>
+
+#include <vtkm/Types.h>
+
+#include <vtkm/testing/Testing.h>
+
+#include <vector>
+
+namespace {
+
+enum TypeId {
+  ID,
+  ID2,
+  ID3,
+  SCALAR,
+  VECTOR2,
+  VECTOR3,
+  VECTOR4
+};
+
+TypeId GetTypeId(vtkm::Id) { return ID; }
+TypeId GetTypeId(vtkm::Id2) { return ID2; }
+TypeId GetTypeId(vtkm::Id3) { return ID3; }
+TypeId GetTypeId(vtkm::Scalar) { return SCALAR; }
+TypeId GetTypeId(vtkm::Vector2) { return VECTOR2; }
+TypeId GetTypeId(vtkm::Vector3) { return VECTOR3; }
+TypeId GetTypeId(vtkm::Vector4) { return VECTOR4; }
+
+struct TestFunctor
+{
+  std::vector<TypeId> FoundTypes;
+
+  template<typename T>
+  VTKM_CONT_EXPORT
+  void operator()(T) {
+    this->FoundTypes.push_back(GetTypeId(T()));
+  }
+};
+
+template<int N>
+void CheckSame(const vtkm::Tuple<TypeId,N> &expected,
+               const std::vector<TypeId> &found)
+{
+  VTKM_TEST_ASSERT(static_cast<int>(found.size()) == N,
+                   "Got wrong number of items.");
+
+  for (int index = 0; index < N; index++)
+  {
+    VTKM_TEST_ASSERT(expected[index] == found[index],
+                     "Got wrong type.");
+  }
+}
+
+template<int N, typename ListTag>
+void TryList(const vtkm::Tuple<TypeId,N> &expected, ListTag)
+{
+  TestFunctor functor;
+  vtkm::ListForEach(functor, ListTag());
+  CheckSame(expected, functor.FoundTypes);
+}
+
+void TestLists()
+{
+  std::cout << "TypeListTagId" << std::endl;
+  TryList(vtkm::Tuple<TypeId,1>(ID), vtkm::TypeListTagId());
+
+  std::cout << "TypeListTagId2" << std::endl;
+  TryList(vtkm::Tuple<TypeId,1>(ID2), vtkm::TypeListTagId2());
+
+  std::cout << "TypeListTagId3" << std::endl;
+  TryList(vtkm::Tuple<TypeId,1>(ID3), vtkm::TypeListTagId3());
+
+  std::cout << "TypeListTagScalar" << std::endl;
+  TryList(vtkm::Tuple<TypeId,1>(SCALAR), vtkm::TypeListTagScalar());
+
+  std::cout << "TypeListTagVector2" << std::endl;
+  TryList(vtkm::Tuple<TypeId,1>(VECTOR2), vtkm::TypeListTagVector2());
+
+  std::cout << "TypeListTagVector3" << std::endl;
+  TryList(vtkm::Tuple<TypeId,1>(VECTOR3), vtkm::TypeListTagVector3());
+
+  std::cout << "TypeListTagVector4" << std::endl;
+  TryList(vtkm::Tuple<TypeId,1>(VECTOR4), vtkm::TypeListTagVector4());
+
+  std::cout << "TypeListTagIndex" << std::endl;
+  TryList(vtkm::Tuple<TypeId,3>(ID,ID2,ID3), vtkm::TypeListTagIndex());
+
+  std::cout << "TypeListTagReal" << std::endl;
+  TryList(vtkm::Tuple<TypeId,4>(SCALAR,VECTOR2,VECTOR3,VECTOR4),
+          vtkm::TypeListTagReal());
+
+  std::cout << "TypeListTagCommon" << std::endl;
+  TryList(vtkm::Tuple<TypeId,3>(ID,SCALAR,VECTOR3), vtkm::TypeListTagCommon());
+
+  std::cout << "TypeListTagAll" << std::endl;
+  vtkm::Tuple<TypeId,7> allTags;
+  allTags[0] = ID;
+  allTags[1] = ID2;
+  allTags[2] = ID3;
+  allTags[3] = SCALAR;
+  allTags[4] = VECTOR2;
+  allTags[5] = VECTOR3;
+  allTags[6] = VECTOR4;
+  TryList(allTags, vtkm::TypeListTagAll());
+}
+
+} // anonymous namespace
+
+int UnitTestTypeListTag(int, char *[])
+{
+  return vtkm::testing::Testing::Run(TestLists);
+}
