267505ff0f7811b0eb7681280120404dda146f8e
Chun-Ming Chen <chenchu@cse.ohio-state.edu>
2015-06-08 12:13:19 -0400

    Remove unncecessary comments

--- vtkm/cont/testing/TestingDeviceAdapter.h
+++ vtkm/cont/testing/TestingDeviceAdapter.h
@@ -1077,7 +1077,7 @@ private:
     }
 
 
-    //next test with values in vec3d (Added by Jimmy to be reviewed)
+    //next test with values in vec3d
     {
     const vtkm::Id inputLength = 3;
     const vtkm::Id expectedLength = 1;
@@ -1117,8 +1117,7 @@ private:
       }
     }
 
-    // ** has problem
-    //next test with values in zip (Added by Jimmy to be reviewed)
+    //next test with values in zip
     {
     const vtkm::Id inputLength = 3;
     const vtkm::Id expectedLength = 1;
@@ -1166,8 +1165,7 @@ private:
       }
     }
 
-    // ** has problem
-    //next test with values in heterogeneous zip (Added by Jimmy to be reviewed)
+    //next test with values in heterogeneous zip
     {
     const vtkm::Id inputLength = 3;
     const vtkm::Id expectedLength = 1;
