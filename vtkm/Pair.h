ee5c47c5eb44a31fc6adcceb65b0604db99f22e1
Chun-Ming Chen <chenchu@cse.ohio-state.edu>
2015-06-08 12:09:53 -0400

    Move Pairwise Add to vtkm namespace

--- vtkm/Pair.h
+++ vtkm/Pair.h
@@ -131,6 +131,16 @@ struct Pair
   }
 };
 
+/// Pairwise Add.
+/// This is done by adding the two objects separately.
+/// Useful for Reduce operation on a zipped array
+template<typename T, typename U>
+VTKM_EXEC_CONT_EXPORT
+vtkm::Pair<T, U> operator+(const vtkm::Pair<T, U>& a, const vtkm::Pair<T, U> &b)
+{
+    return vtkm::Pair<T,U>(a.first + b.first, a.second + b.second);
+}
+
 template <typename T1, typename T2>
 VTKM_EXEC_CONT_EXPORT
 vtkm::Pair<T1,T2> make_Pair(const T1 &firstSrc, const T2 &secondSrc)
@@ -139,12 +149,5 @@ vtkm::Pair<T1,T2> make_Pair(const T1 &firstSrc, const T2 &secondSrc)
 }
 
 } // namespace vtkm
-
-
-template<typename T, typename U>
-vtkm::Pair<T, U> operator+(const vtkm::Pair<T, U>& a, const vtkm::Pair<T, U> &b)
-{
-    return vtkm::Pair<T,U>(a.first + b.first, a.second + b.second);
-}
 
 #endif //vtk_m_Pair_h
