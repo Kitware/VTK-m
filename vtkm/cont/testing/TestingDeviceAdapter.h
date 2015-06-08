d42edb51ab4fc82f2f312a0c8a048ee504caa106
Chun-Ming Chen <chenchu@cse.ohio-state.edu>
2015-06-08 13:31:31 -0400

    Include ArrayHandleZip in related testing codes

--- vtkm/cont/testing/TestingDeviceAdapter.h
+++ vtkm/cont/testing/TestingDeviceAdapter.h
@@ -25,6 +25,7 @@
 #include <vtkm/cont/ArrayHandleZip.h>
 #include <vtkm/cont/ArrayPortalToIterators.h>
 #include <vtkm/cont/ArrayHandleConstant.h>
+#include <vtkm/cont/ArrayHandleZip.h>
 #include <vtkm/cont/ErrorControlOutOfMemory.h>
 #include <vtkm/cont/ErrorExecution.h>
 #include <vtkm/cont/StorageBasic.h>
