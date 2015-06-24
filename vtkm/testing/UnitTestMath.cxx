b6423384654328bc7e3e1a9f3b65a5cc3bb07c90
Kenneth Moreland <kmorel@sandia.gov>
2015-06-24 16:31:57 -0600

    Fix copyright statement in UnitTestMath.cxx.

--- vtkm/testing/UnitTestMath.cxx
+++ vtkm/testing/UnitTestMath.cxx
@@ -8,9 +8,15 @@
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //  PURPOSE.  See the above copyright notice for more information.
 //
-//  Copyright 2012 Sandia Corporation.
+//  Copyright 2015 Sandia Corporation.
+//  Copyright 2015 UT-Battelle, LLC.
+//  Copyright 2015 Los Alamos National Security.
+//
 //  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 //  the U.S. Government retains certain rights in this software.
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
 //
 //=============================================================================
 
