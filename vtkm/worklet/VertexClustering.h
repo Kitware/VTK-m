00382a3b8b390ca6052e253b1028523a817db46b
Chun-Ming Chen <chenchu@cse.ohio-state.edu>
2015-06-25 15:33:25 -0400

    fix warning: && with ||

--- vtkm/worklet/VertexClustering.h
+++ vtkm/worklet/VertexClustering.h
@@ -203,8 +203,8 @@ struct VertexClustering{
         return false;
       return b[0] < 0 ||
              a[0] < b[0] ||
-             a[0]==b[0] && a[1] < b[1] ||
-             a[0]==b[0] && a[1]==b[1] && a[2] < b[2];
+             (a[0]==b[0] && a[1] < b[1]) ||
+             (a[0]==b[0] && a[1]==b[1] && a[2] < b[2]);
     }
   };
 
