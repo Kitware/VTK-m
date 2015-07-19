77ec7432194029cf93d4290fdc773cadbca0e350
Brent Lessley <blessley@cs.uoregon.edu>
2015-07-18 17:50:36 -0700

    Resolved all compilation errors and warnings.  Replaced the nested for-loops of the face connectivity construction with an implicit array handle invocation.

--- vtkm/worklet/testing/UnitTestExternalFaces.cxx
+++ vtkm/worklet/testing/UnitTestExternalFaces.cxx
@@ -28,101 +28,101 @@
 vtkm::cont::DataSet RunExternalFaces(vtkm::cont::DataSet &ds)
 {
 
-  boost::shared_ptr<vtkm::cont::CellSet> scs = ds.GetCellSet(0);
-  vtkm::cont::CellSetExplicit<> *cs =
-      dynamic_cast<vtkm::cont::CellSetExplicit<> *>(scs.get());
+      boost::shared_ptr<vtkm::cont::CellSet> scs = ds.GetCellSet(0);
+      vtkm::cont::CellSetExplicit<> *cs =
+          dynamic_cast<vtkm::cont::CellSetExplicit<> *>(scs.get());
 
-  vtkm::cont::ArrayHandle<vtkm::Id> shapes = cs->GetNodeToCellConnectivity().GetShapesArray();
-  vtkm::cont::ArrayHandle<vtkm::Id> numIndices = cs->GetNodeToCellConnectivity().GetNumIndicesArray();
-  vtkm::cont::ArrayHandle<vtkm::Id> conn = cs->GetNodeToCellConnectivity().GetConnectivityArray();
+      vtkm::cont::ArrayHandle<vtkm::Id> shapes = cs->GetNodeToCellConnectivity().GetShapesArray();
+      vtkm::cont::ArrayHandle<vtkm::Id> numIndices = cs->GetNodeToCellConnectivity().GetNumIndicesArray();
+      vtkm::cont::ArrayHandle<vtkm::Id> conn = cs->GetNodeToCellConnectivity().GetConnectivityArray();
 
-  vtkm::cont::ArrayHandle<vtkm::Id> output_shapes;
-  vtkm::cont::ArrayHandle<vtkm::Id> output_numIndices;
-  vtkm::cont::ArrayHandle<vtkm::Id> output_conn;
+      vtkm::cont::ArrayHandle<vtkm::Id> output_shapes;
+      vtkm::cont::ArrayHandle<vtkm::Id> output_numIndices;
+      vtkm::cont::ArrayHandle<vtkm::Id> output_conn;
 
-  //Run the External Faces worklet
-  vtkm::worklet::ExternalFaces<VTKM_DEFAULT_DEVICE_ADAPTER_TAG>().run(
-        shapes,
-        numIndices,
-        conn,
-        output_shapes,
-        output_numIndices,
-        output_conn);
+      //Run the External Faces worklet
+      vtkm::worklet::ExternalFaces<VTKM_DEFAULT_DEVICE_ADAPTER_TAG>().run(
+            shapes,
+            numIndices,
+            conn,
+            output_shapes,
+            output_numIndices,
+            output_conn);
 
-  vtkm::cont::DataSet new_ds;
-  new_ds.AddField(ds.GetField("x"));
-  new_ds.AddField(ds.GetField("y"));
-  new_ds.AddField(ds.GetField("z"));
-  new_ds.AddCoordinateSystem(vtkm::cont::CoordinateSystem("x","y","z"));
-  boost::shared_ptr< vtkm::cont::CellSetExplicit<> > new_cs(
-                              new vtkm::cont::CellSetExplicit<>("cells", output_shapes.GetNumberOfValues()));
-  vtkm::cont::ExplicitConnectivity<> &new_ec = new_cs->nodesOfCellsConnectivity;
-  new_ec.Fill(output_shapes, output_numIndices, output_conn);
-  new_ds.AddCellSet(new_cs);
+      vtkm::cont::DataSet new_ds;
+      new_ds.AddField(ds.GetField("x"));
+      new_ds.AddField(ds.GetField("y"));
+      new_ds.AddField(ds.GetField("z"));
+      new_ds.AddCoordinateSystem(vtkm::cont::CoordinateSystem("x","y","z"));
+      boost::shared_ptr< vtkm::cont::CellSetExplicit<> > new_cs(
+                                  new vtkm::cont::CellSetExplicit<>("cells", output_shapes.GetNumberOfValues()));
+      vtkm::cont::ExplicitConnectivity<> &new_ec = new_cs->nodesOfCellsConnectivity;
+      new_ec.Fill(output_shapes, output_numIndices, output_conn);
+      new_ds.AddCellSet(new_cs);
 
-  return new_ds;
+      return new_ds;
 }
 
 void TestExternalFaces()
 {
-  //--------------Construct a VTK-m Test Dataset----------------
+      //--------------Construct a VTK-m Test Dataset----------------
 
-  vtkm::cont::DataSet ds;
+      vtkm::cont::DataSet ds;
 
-  const int nVerts = 8; //A cube that is tetrahedralized
-  vtkm::Float32 xVals[nVerts] = {0, 1, 1, 0, 0, 1, 1, 0};
-  vtkm::Float32 yVals[nVerts] = {0, 0, 1, 1, 0, 0, 1, 1};
-  vtkm::Float32 zVals[nVerts] = {0, 0, 0, 0, 1, 1, 1, 1};
-  ds.AddField(vtkm::cont::Field("x", 1, vtkm::cont::Field::ASSOC_POINTS, xVals, nVerts));
-  ds.AddField(vtkm::cont::Field("y", 1, vtkm::cont::Field::ASSOC_POINTS, yVals, nVerts));
-  ds.AddField(vtkm::cont::Field("z", 1, vtkm::cont::Field::ASSOC_POINTS, zVals, nVerts));
-  ds.AddCoordinateSystem(vtkm::cont::CoordinateSystem("x","y","z"));
+      const int nVerts = 8; //A cube that is tetrahedralized
+      vtkm::Float32 xVals[nVerts] = {0, 1, 1, 0, 0, 1, 1, 0};
+      vtkm::Float32 yVals[nVerts] = {0, 0, 1, 1, 0, 0, 1, 1};
+      vtkm::Float32 zVals[nVerts] = {0, 0, 0, 0, 1, 1, 1, 1};
+      ds.AddField(vtkm::cont::Field("x", 1, vtkm::cont::Field::ASSOC_POINTS, xVals, nVerts));
+      ds.AddField(vtkm::cont::Field("y", 1, vtkm::cont::Field::ASSOC_POINTS, yVals, nVerts));
+      ds.AddField(vtkm::cont::Field("z", 1, vtkm::cont::Field::ASSOC_POINTS, zVals, nVerts));
+      ds.AddCoordinateSystem(vtkm::cont::CoordinateSystem("x","y","z"));
 
-  //Construct the VTK-m shapes and numIndices connectivity arrays
-  const int nCells = 6;  //The tetrahedrons of the cube
-  int cellVerts[nCells][4] = {{4,7,6,3}, {4,6,3,2}, {4,0,3,2},
-                             {4,6,5,2}, {4,5,0,2}, {1,0,5,2}};
-  boost::shared_ptr< vtkm::cont::CellSetExplicit<> > cs(
-                              new vtkm::cont::CellSetExplicit<>("cells", nCells));
-  vtkm::cont::ArrayHandle<vtkm::Id> shapes;
-  vtkm::cont::ArrayHandle<vtkm::Id> numIndices;
-  vtkm::cont::ArrayHandle<vtkm::Id> conn;
-  shapes.Allocate(static_cast<vtkm::Id>(nCells));
-  numIndices.Allocate(static_cast<vtkm::Id>(nCells));
-  conn.Allocate(static_cast<vtkm::Id>(4 * nCells));
+      //Construct the VTK-m shapes and numIndices connectivity arrays
+      const int nCells = 6;  //The tetrahedrons of the cube
+      int cellVerts[nCells][4] = {{4,7,6,3}, {4,6,3,2}, {4,0,3,2},
+                                 {4,6,5,2}, {4,5,0,2}, {1,0,5,2}};
+      boost::shared_ptr< vtkm::cont::CellSetExplicit<> > cs(
+                                  new vtkm::cont::CellSetExplicit<>("cells", nCells));
+      vtkm::cont::ArrayHandle<vtkm::Id> shapes;
+      vtkm::cont::ArrayHandle<vtkm::Id> numIndices;
+      vtkm::cont::ArrayHandle<vtkm::Id> conn;
+      shapes.Allocate(static_cast<vtkm::Id>(nCells));
+      numIndices.Allocate(static_cast<vtkm::Id>(nCells));
+      conn.Allocate(static_cast<vtkm::Id>(4 * nCells));
 
-  int index = 0;
-  for(int j = 0; j < nCells; j++)
-  {
-      shapes.GetPortalControl().Set(j, static_cast<vtkm::Id>(vtkm::VTKM_TETRA));
-      numIndices.GetPortalControl().Set(j, 4);
-      for(int k = 0; k < 4; k++)
-        conn.GetPortalControl().Set(index++, static_cast<vtkm::Id>(cellVerts[j][k]));
-  }
+      int index = 0;
+      for(int j = 0; j < nCells; j++)
+      {
+          shapes.GetPortalControl().Set(j, static_cast<vtkm::Id>(vtkm::VTKM_TETRA));
+          numIndices.GetPortalControl().Set(j, 4);
+          for(int k = 0; k < 4; k++)
+            conn.GetPortalControl().Set(index++, static_cast<vtkm::Id>(cellVerts[j][k]));
+      }
 
-  vtkm::cont::ExplicitConnectivity<> &ec = cs->nodesOfCellsConnectivity;
-  ec.Fill(shapes, numIndices, conn);
+      vtkm::cont::ExplicitConnectivity<> &ec = cs->nodesOfCellsConnectivity;
+      ec.Fill(shapes, numIndices, conn);
 
-  //Add the VTK-m cell set
-  ds.AddCellSet(cs);
-  ds.PrintSummary(std::cout);
+      //Add the VTK-m cell set
+      ds.AddCellSet(cs);
+      ds.PrintSummary(std::cout);
 
-  //Run the External Faces worklet
-  vtkm::cont::DataSet new_ds = RunExternalFaces(ds);
-  boost::shared_ptr<vtkm::cont::CellSet> new_scs = new_ds.GetCellSet(0);
-  vtkm::cont::CellSetExplicit<> *new_cs =
-      dynamic_cast<vtkm::cont::CellSetExplicit<> *>(new_scs.get());
+      //Run the External Faces worklet
+      vtkm::cont::DataSet new_ds = RunExternalFaces(ds);
+      boost::shared_ptr<vtkm::cont::CellSet> new_scs = new_ds.GetCellSet(0);
+      vtkm::cont::CellSetExplicit<> *new_cs =
+          dynamic_cast<vtkm::cont::CellSetExplicit<> *>(new_scs.get());
 
-  vtkm::Id numExtFaces_out = new_cs->GetNumCells();
+      vtkm::Id numExtFaces_out = new_cs->GetNumCells();
 
-  //Validate the number of external faces (output) returned by the worklet
-  const vtkm::Id numExtFaces_actual = 12;
-  VTKM_TEST_ASSERT(numExtFaces_out == numExtFaces_actual, "Number of External Faces mismatch");
+      //Validate the number of external faces (output) returned by the worklet
+      const vtkm::Id numExtFaces_actual = 12;
+      VTKM_TEST_ASSERT(numExtFaces_out == numExtFaces_actual, "Number of External Faces mismatch");
 
 } // TestExternalFaces
 
 
 int UnitTestExternalFaces(int, char *[])
 {
-  return vtkm::cont::testing::Testing::Run(TestExternalFaces);
+      return vtkm::cont::testing::Testing::Run(TestExternalFaces);
 }
