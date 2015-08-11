2abf599c429d14ff53ea7253973488ba03ef92da
hschroot <hschroots@ucdavis.edu>
2015-08-11 14:59:44 -0700

    Add OpenGL Interop Functionality
    Copy the OpenGL Interop code from DAX and convert it to VTKm.
    Tests include transferring data to an OpenGL context for all VTKm common
    data types.
    
    A WindowBase class using GLUT is also included.

---
+++ vtkm/opengl/internal/TransferToOpenGL.h
@@ -0,0 +1,161 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2014 Sandia Corporation.
+//  Copyright 2014 UT-Battelle, LLC.
+//  Copyright 2014 Los Alamos National Security.
+//
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
+//============================================================================
+#ifndef vtk_m_opengl_internal_TransferToOpenGL_h
+#define vtk_m_opengl_internal_TransferToOpenGL_h
+
+#include <vtkm/cont/ArrayHandle.h>
+#include <vtkm/cont/StorageBasic.h>
+
+#include <vtkm/opengl/internal/OpenGLHeaders.h>
+#include <vtkm/opengl/internal/BufferTypePicker.h>
+namespace vtkm {
+namespace opengl {
+namespace internal {
+
+namespace detail
+{
+
+template<class ValueType, class StorageTag, class DeviceAdapterTag>
+VTKM_CONT_EXPORT
+void CopyFromHandle(  
+  vtkm::cont::ArrayHandle<ValueType, StorageTag>& handle,
+  GLenum type,
+  DeviceAdapterTag)
+{
+  //Generic implementation that will work no matter what. We pull the data
+  //back to the control enviornment using PerpareForInput and give an iterator
+  //from the portal to OpenGL to upload to the rendering system
+  const vtkm::Id numberOfValues = handle.GetNumberOfValues();
+  std::size_t size = sizeof(ValueType) * numberOfValues;
+
+  //Copy that data its specialized Storage container to a basic storage
+  vtkm::cont::ArrayHandle<ValueType, vtkm::cont::StorageTagBasic> tmpHandle;
+  vtkm::cont::DeviceAdapterAlgorithm<DeviceAdapterTag>::Copy(handle, tmpHandle);
+
+  //Sync the control array to make sure the most current data is available
+  tmpHandle.SyncControlArray();
+
+  //Note that the ArrayHandle is no longer valid after this call
+  ValueType* temporaryStorage = tmpHandle.Internals->ControlArray.StealArray();
+
+  //Detach the current buffer
+  glBufferData(type, size, 0, GL_DYNAMIC_DRAW);
+  
+  //Allocate the memory and set it as static draw and copy into opengl
+  glBufferSubData(type,0,size,temporaryStorage);
+
+  delete[] temporaryStorage;
+}
+
+template<class ValueType, class DeviceAdapterTag>
+VTKM_CONT_EXPORT
+void CopyFromHandle(  
+  vtkm::cont::ArrayHandle<ValueType, vtkm::cont::StorageTagBasic>& handle,
+  GLenum type,
+  DeviceAdapterTag)
+{
+  //Specialization given that we are use an C allocated array storage tag
+  //that allows us to directly hook into the data. We pull the data
+  //back to the control enviornment using PerpareForInput and give an iterator
+  //from the portal to OpenGL to upload to the rendering system
+  //This also works because we know that this class isn't used for cuda interop, 
+  //instead we are specialized
+  std::size_t size = sizeof(ValueType) * handle.GetNumberOfValues();
+
+  //Detach the current buffer
+  glBufferData(type, size, 0, GL_DYNAMIC_DRAW);
+
+  //Allocate the memory and set it as static draw and copy into opengl
+  glBufferSubData(type,0,size,
+                      vtkm::cont::ArrayPortalToIteratorBegin(
+                          handle.PrepareForInput(DeviceAdapterTag())
+                  ));
+}
+
+} //namespace detail
+
+/// \brief Manages transferring an ArrayHandle to opengl .
+///
+/// \c TransferToOpenGL manages to transfer the contents of an ArrayHandle
+/// to OpenGL as efficiently as possible.
+///
+template<typename ValueType, class DeviceAdapterTag>
+class TransferToOpenGL
+{
+public:
+  VTKM_CONT_EXPORT TransferToOpenGL():
+  Type( vtkm::opengl::internal::BufferTypePicker( ValueType() ) )
+  {}
+
+  VTKM_CONT_EXPORT explicit TransferToOpenGL(GLenum type):
+  Type(type)
+  {}
+
+  VTKM_CONT_EXPORT GLenum GetType() const { return this->Type; }
+
+  template< typename StorageTag >
+  VTKM_CONT_EXPORT
+  void Transfer (
+    vtkm::cont::ArrayHandle<ValueType, StorageTag>& handle,
+    GLuint& openGLHandle ) const
+  {
+  //make a buffer for the handle if the user has forgotten too
+  if(!glIsBuffer(openGLHandle))
+    {
+    glGenBuffers(1,&openGLHandle);
+    }
+
+  //bind the buffer to the given buffer type
+  glBindBuffer(this->Type, openGLHandle);
+
+  //transfer the data.
+  //the primary concern that we have at this point is data locality and
+  //the type of storage. Our options include using CopyInto and provide a
+  //temporary location for the values to reside before we give it to openGL
+  //this works for all storage types.
+  //
+  //Second option is to call PrepareForInput and get a PortalConst in the
+  //exection environment.
+  //if we are StorageTagBasic this would allow us the ability to grab
+  //the raw memory value and copy those, which we know are valid and remove
+  //a unneeded copy.
+  //
+  //The end result is that we have CopyFromHandle which does number two
+  //from StorageTagBasic, and does the CopyInto for everything else
+  detail::CopyFromHandle(handle, this->Type, DeviceAdapterTag());
+  }
+private:
+  GLenum Type;
+};
+
+}
+}
+} //namespace vtkm::opengl::internal
+
+//-----------------------------------------------------------------------------
+// These includes are intentionally placed here after the declaration of the
+// TransferToOpenGL class, so that people get the correct device adapter
+/// specializations if they exist.
+#if VTKM_DEVICE_ADAPTER == VTKM_DEVICE_ADAPTER_CUDA
+#include <vtkm/opengl/cuda/internal/TransferToOpenGL.h>
+#endif
+
+
+#endif //vtk_m_opengl_internal_TransferToOpenGL_h
---
+++ vtkm/opengl/testing/WindowBase.h
@@ -0,0 +1,160 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2014 Sandia Corporation.
+//  Copyright 2014 UT-Battelle, LLC.
+//  Copyright 2014 Los Alamos National Security.
+//
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
+//============================================================================
+#ifndef vtkm_m_opengl_testing_WindowBase_h
+#define vtkm_m_opengl_testing_WindowBase_h
+
+//constructs a valid openGL context so that we can verify
+//that vtkm to open gl bindings work
+#include <string>
+
+// OpenGL Graphics includes
+//glew needs to go before glut
+#include <vtkm/opengl/internal/OpenGLHeaders.h>
+#if defined (__APPLE__)
+# include <GLUT/glut.h>
+#else
+# include <GL/glut.h>
+#endif
+
+#include <vtkm/cont/ErrorControlBadValue.h>
+
+#ifdef VTKM_CUDA
+# include <vtkm/cont/cuda/ChooseCudaDevice.h>
+# include <vtkm/opengl/cuda/SetOpenGLDevice.h>
+#endif
+
+#include <iostream>
+
+
+namespace vtkm{
+namespace opengl{
+namespace testing{
+
+namespace internal
+{
+template <typename T>
+struct GLUTStaticCallbackHolder
+{ static T* StaticGLUTResource; };
+
+template <typename T>
+T* GLUTStaticCallbackHolder<T>::StaticGLUTResource;
+
+}
+
+
+/// \brief Basic GLUT Wrapper class
+///
+/// This class gives the ability to wrap the glut function callbacks into
+/// a single class so that you can use c++ objects. The only downside
+/// is that you can only have a single window created
+///
+template< class Derived >
+class WindowBase : private internal::GLUTStaticCallbackHolder<Derived>
+{
+
+public:
+  void Init(std::string title, int width, int height,
+            int argc, char** argv)
+  {
+  //set our selves as the static instance to call
+  WindowBase<Derived>::StaticGLUTResource = static_cast<Derived*>(this);
+
+  glutInit(&argc,argv);
+  glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+  glutInitWindowPosition(0,0);
+  glutInitWindowSize(width,height);
+  glutCreateWindow(title.c_str());
+
+  // glewExperimental = GL_TRUE;
+  glewInit();
+
+  if(!glewIsSupported("GL_VERSION_2_1"))
+    {
+    std::cerr << glGetString(GL_RENDERER) << std::endl;
+    std::cerr << glGetString(GL_VERSION) << std::endl;
+    throw vtkm::cont::ErrorControlBadValue(
+                                  "Unable to create an OpenGL 2.1 Context");
+    }
+
+#ifdef VTKM_CUDA
+    int id = vtkm::cuda::cont::FindFastestDeviceId();
+    vtkm::opengl::cuda::SetCudaGLDevice(id);
+#endif
+
+  //attach all the glut call backs
+  glutDisplayFunc( WindowBase<Derived>::GLUTDisplayCallback );
+  glutIdleFunc( WindowBase<Derived>::GLUTIdleCallback );
+  glutReshapeFunc( WindowBase<Derived>::GLUTChangeSizeCallback );
+  glutKeyboardFunc( WindowBase<Derived>::GLUTKeyCallback );
+  glutSpecialFunc( WindowBase<Derived>::GLUTSpecialKeyCallback );
+  glutMouseFunc( WindowBase<Derived>::GLUTMouseCallback );
+  glutMotionFunc( WindowBase<Derived>::GLUTMouseMoveCallback );
+  glutPassiveMotionFunc( WindowBase<Derived>::GLUTPassiveMouseMoveCallback );
+
+  //call any custom init code you want to have
+  WindowBase<Derived>::StaticGLUTResource->PostInit();
+  }
+
+  void Init(std::string title, int width, int height)
+  {
+    int argc=0;
+    char** argv = 0;
+    Init(title,width,height,argc,argv);
+  }
+
+  //Init must be called before you call Start so that we have a valid
+  //opengl context
+  void Start()
+  {
+    glutMainLoop();
+  }
+
+
+  static void GLUTDisplayCallback()
+    { WindowBase<Derived>::StaticGLUTResource->Display(); }
+
+  static void GLUTIdleCallback()
+    { WindowBase<Derived>::StaticGLUTResource->Idle(); }
+
+  static void GLUTChangeSizeCallback(int width, int height)
+    { WindowBase<Derived>::StaticGLUTResource->ChangeSize(width,height); }
+
+  static void GLUTKeyCallback(unsigned char key, int x, int y)
+    { WindowBase<Derived>::StaticGLUTResource->Key(key,x,y); }
+
+  static void GLUTSpecialKeyCallback(int key, int x, int y)
+    { WindowBase<Derived>::StaticGLUTResource->SpecialKey(key,x,y); }
+
+  static void GLUTMouseCallback(int button, int state ,int x, int y)
+    { WindowBase<Derived>::StaticGLUTResource->Mouse(button,state,x,y); }
+
+  static void GLUTMouseMoveCallback(int x, int y)
+    { WindowBase<Derived>::StaticGLUTResource->MouseMove(x,y); }
+
+  static void GLUTPassiveMouseMoveCallback(int x, int y)
+    { WindowBase<Derived>::StaticGLUTResource->PassiveMouseMove(x,y); }
+
+};
+
+
+}
+}
+}
+#endif //vtkm_m_opengl_testing_WindowBase_h
