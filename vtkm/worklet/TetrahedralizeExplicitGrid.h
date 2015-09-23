4e92cd3f33de47a651092a3a3d2ecd6191fbf689
Patricia Kroll Fasel - 090207 <pkf@darwin-login2.lanl.gov>
2015-09-23 14:43:27 -0600

    Tetrahedralize 2D explicit dataset first version with opengl example

---
+++ vtkm/worklet/TetrahedralizeExplicitGrid.h
@@ -0,0 +1,210 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2014 Sandia Corporation.
+//  Copyright 2014 UT-Battelle, LLC.
+//  Copyright 2014 Los Alamos National Security.
+//
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
+//============================================================================
+
+#ifndef vtk_m_worklet_TetrahedralizeExplicitGrid_h
+#define vtk_m_worklet_TetrahedralizeExplicitGrid_h
+
+#include <vtkm/cont/DeviceAdapter.h>
+#include <vtkm/cont/ArrayHandle.h>
+#include <vtkm/cont/ArrayHandleCounting.h>
+#include <vtkm/cont/DynamicArrayHandle.h>
+#include <vtkm/cont/DataSet.h>
+#include <vtkm/cont/CellSetExplicit.h>
+#include <vtkm/cont/Field.h>
+
+#include <vtkm/worklet/DispatcherMapField.h>
+#include <vtkm/worklet/WorkletMapField.h>
+#include <vtkm/worklet/DispatcherMapTopology.h>
+#include <vtkm/worklet/WorkletMapTopology.h>
+
+#include <vtkm/exec/ExecutionWholeArray.h>
+
+namespace vtkm {
+namespace worklet {
+
+/// \brief Compute the tetrahedralize cells for an explicit grid data set
+template <typename DeviceAdapter>
+class TetrahedralizeFilterExplicitGrid
+{
+public:
+
+  //
+  // Worklet to count the number of triangles generated per cell
+  //
+  class TrianglesPerCell : public vtkm::worklet::WorkletMapField
+  {
+  public:
+    typedef void ControlSignature(FieldIn<> shapes,
+                                  FieldIn<> numIndices,
+                                  FieldOut<> triangleCount);
+    typedef void ExecutionSignature(_1,_2,_3);
+    typedef _1 InputDomain;
+
+    VTKM_CONT_EXPORT
+    TrianglesPerCell() {}
+  
+    VTKM_EXEC_EXPORT
+    void operator()(const vtkm::Id &shape, 
+                    const vtkm::Id &numIndices,
+                    vtkm::Id &triangleCount) const
+    {
+      if (shape == vtkm::CELL_SHAPE_TRIANGLE)
+        triangleCount = 1;
+      else if (shape == vtkm::CELL_SHAPE_QUAD)
+        triangleCount = 2;
+      else if (shape == vtkm::CELL_SHAPE_POLYGON)
+        triangleCount = numIndices - 2;
+      else triangleCount = 0;
+    }
+  };
+
+  //
+  // Worklet to turn cells into triangles
+  // Vertices remain the same and each cell is processed with needing topology
+  //
+  class TriangulateCell : public vtkm::worklet::WorkletMapTopologyPointToCell
+  {
+  public:
+    typedef void ControlSignature(FieldInTo<> triangleOffset,
+                                  TopologyIn topology,
+                                  ExecObject connectivity);
+    typedef void ExecutionSignature(_1,_3, CellShape, FromIndices);
+    typedef _2 InputDomain;
+
+    VTKM_CONT_EXPORT
+    TriangulateCell() {}
+
+    // Each cell produces triangles and write result at the offset
+    template<typename CellShapeTag, typename CellNodeVecType>
+    VTKM_EXEC_EXPORT
+    void operator()(vtkm::Id &offset,
+                    vtkm::exec::ExecutionWholeArray<vtkm::Id> &connectivity,
+                    CellShapeTag shape,
+                    const CellNodeVecType &cellNodeIds) const
+    {
+      // Offset is in triangles, 3 vertices per triangle needed
+      vtkm::Id startIndex = offset * 3;
+      if (shape.Id == vtkm::CELL_SHAPE_TRIANGLE) {
+        connectivity.Set(startIndex++, cellNodeIds[0]);
+        connectivity.Set(startIndex++, cellNodeIds[1]);
+        connectivity.Set(startIndex++, cellNodeIds[2]);
+
+      } else if (shape.Id == vtkm::CELL_SHAPE_QUAD) {
+        connectivity.Set(startIndex++, cellNodeIds[0]);
+        connectivity.Set(startIndex++, cellNodeIds[1]);
+        connectivity.Set(startIndex++, cellNodeIds[2]);
+
+        connectivity.Set(startIndex++, cellNodeIds[0]);
+        connectivity.Set(startIndex++, cellNodeIds[2]);
+        connectivity.Set(startIndex++, cellNodeIds[3]);
+
+      } else if (shape.Id == vtkm::CELL_SHAPE_POLYGON) {
+        for (vtkm::Id tri = 0; tri < 4; tri++) {
+          connectivity.Set(startIndex++, cellNodeIds[0]);
+          connectivity.Set(startIndex++, cellNodeIds[tri+1]);
+          connectivity.Set(startIndex++, cellNodeIds[tri+2]);
+        }
+      }
+    }
+  };
+
+  //
+  // Construct the filter to tetrahedralize explicit grid
+  //
+  TetrahedralizeFilterExplicitGrid(const vtkm::cont::DataSet &inDataSet,
+                                         vtkm::cont::DataSet &outDataSet) :
+    InDataSet(inDataSet),
+    OutDataSet(outDataSet)
+  {
+  }
+
+  vtkm::cont::DataSet InDataSet;  // input dataset with structured cell set
+  vtkm::cont::DataSet OutDataSet; // output dataset with explicit cell set
+
+  //
+  // Populate the output dataset with triangles based on input explicit dataset
+  //
+  void Run()
+  {
+    typedef typename vtkm::cont::DeviceAdapterAlgorithm<DeviceAdapter> DeviceAlgorithms;
+
+    // Cell sets belonging to input and output datasets
+    vtkm::cont::CellSetExplicit<> &inCellSet =
+      this->InDataSet.GetCellSet(0).CastTo<vtkm::cont::CellSetExplicit<> >();
+    vtkm::cont::CellSetExplicit<> &cellSet = 
+      this->OutDataSet.GetCellSet(0).CastTo<vtkm::cont::CellSetExplicit<> >();
+
+    // Input dataset vertices and cell counts
+    vtkm::Id numberOfInCells = inCellSet.GetNumberOfCells();
+
+    // Input topology
+    vtkm::cont::ArrayHandle<vtkm::Id> inShapes = inCellSet.GetShapesArray(
+      vtkm::TopologyElementTagPoint(), vtkm::TopologyElementTagCell());
+    vtkm::cont::ArrayHandle<vtkm::Id> inNumIndices = inCellSet.GetNumIndicesArray(
+      vtkm::TopologyElementTagPoint(), vtkm::TopologyElementTagCell());
+    vtkm::cont::ArrayHandle<vtkm::Id> inConn = inCellSet.GetConnectivityArray(
+      vtkm::TopologyElementTagPoint(), vtkm::TopologyElementTagCell());
+
+    // Cell indices are just counting array
+    //vtkm::cont::ArrayHandleCounting<vtkm::Id> cellIndicesArray(0, 1, numberOfInCells);
+
+    // Determine the number of triangles each cell will generate
+    vtkm::cont::ArrayHandle<vtkm::Id> trianglesPerCellArray;
+    vtkm::worklet::DispatcherMapField<TrianglesPerCell> trianglesPerCellDispatcher;
+    trianglesPerCellDispatcher.Invoke(inShapes, inNumIndices, trianglesPerCellArray);
+
+    // Number of triangles and number of vertices needed
+    vtkm::cont::ArrayHandle<vtkm::Id> triangleOffset;
+    vtkm::Id numberOfOutCells = DeviceAlgorithms::ScanExclusive(trianglesPerCellArray,
+                                                                triangleOffset);
+    vtkm::Id numberOfOutIndices = numberOfOutCells * 3;
+
+    // Information needed to build the output cell set
+    vtkm::cont::ArrayHandle<vtkm::Id> shapes;
+    vtkm::cont::ArrayHandle<vtkm::Id> numIndices;
+    vtkm::cont::ArrayHandle<vtkm::Id> connectivity;
+
+    shapes.Allocate(static_cast<vtkm::Id>(numberOfOutCells));
+    numIndices.Allocate(static_cast<vtkm::Id>(numberOfOutCells));
+    connectivity.Allocate(static_cast<vtkm::Id>(numberOfOutIndices));
+
+    // Fill the arrays of shapes and number of indices needed by the cell set
+    for (vtkm::Id j = 0; j < numberOfOutCells; j++) {
+      shapes.GetPortalControl().Set(j, static_cast<vtkm::Id>(vtkm::CELL_SHAPE_TRIANGLE));
+      numIndices.GetPortalControl().Set(j, 3);
+    }
+
+    // Call the TriangulateCell functor to compute the triangle connectivity
+    vtkm::worklet::DispatcherMapTopology<TriangulateCell> triangulateCellDispatcher;
+    triangulateCellDispatcher.Invoke(
+                      triangleOffset,
+                      inCellSet,
+                      vtkm::exec::ExecutionWholeArray<vtkm::Id>(connectivity, numberOfOutIndices));
+
+    // Add tets to output cellset
+    cellSet.Fill(shapes, numIndices, connectivity);
+  }
+
+};
+
+}
+} // namespace vtkm::worklet
+
+#endif // vtk_m_worklet_TetrahedralizeExplicitGrid_h
---
+++ vtkm/worklet/testing/UnitTestTetrahedralizeExplicitGrid.cxx
@@ -0,0 +1,180 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2014 Sandia Corporation.
+//  Copyright 2014 UT-Battelle, LLC.
+//  Copyright 2014 Los Alamos National Security.
+//
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
+//============================================================================
+
+#include <vtkm/worklet/TetrahedralizeExplicitGrid.h>
+#include <vtkm/worklet/DispatcherMapField.h>
+
+#include <vtkm/cont/CellSetExplicit.h>
+#include <vtkm/cont/DataSet.h>
+
+#include <vtkm/cont/testing/Testing.h>
+
+namespace {
+
+//
+// Test 2D explicit dataset
+//
+vtkm::cont::DataSet MakeTriangulateExplicitDataSet()
+{
+  vtkm::cont::DataSet dataSet;
+
+  const int nVerts = 16;
+  typedef vtkm::Vec<vtkm::Float32,3> CoordType;
+  CoordType coordinates[nVerts] = {
+    CoordType(0, 0, 0),   // 0
+    CoordType(1, 0, 0),   // 1
+    CoordType(2, 0, 0),   // 2
+    CoordType(3, 0, 0),   // 3
+    CoordType(0, 1, 0),   // 4
+    CoordType(1, 1, 0),   // 5
+    CoordType(2, 1, 0),   // 6
+    CoordType(3, 1, 0),   // 7
+    CoordType(0, 2, 0),   // 8
+    CoordType(1, 2, 0),   // 9
+    CoordType(2, 2, 0),   // 10
+    CoordType(3, 2, 0),   // 11
+    CoordType(0, 3, 0),   // 12
+    CoordType(3, 3, 0),   // 13
+    CoordType(1, 4, 0),   // 14
+    CoordType(2, 4, 0),   // 15
+  };
+
+  dataSet.AddCoordinateSystem(
+          vtkm::cont::CoordinateSystem("coordinates", 1, coordinates, nVerts));
+
+  std::vector<vtkm::Id> shapes;
+  shapes.push_back(vtkm::CELL_SHAPE_TRIANGLE);
+  shapes.push_back(vtkm::CELL_SHAPE_QUAD);
+  shapes.push_back(vtkm::CELL_SHAPE_QUAD);
+  shapes.push_back(vtkm::CELL_SHAPE_QUAD);
+  shapes.push_back(vtkm::CELL_SHAPE_TRIANGLE);
+  shapes.push_back(vtkm::CELL_SHAPE_QUAD);
+  shapes.push_back(vtkm::CELL_SHAPE_POLYGON);
+
+  std::vector<vtkm::Id> numindices;
+  numindices.push_back(3);
+  numindices.push_back(4);
+  numindices.push_back(4);
+  numindices.push_back(4);
+  numindices.push_back(3);
+  numindices.push_back(4);
+  numindices.push_back(6);
+
+  std::vector<vtkm::Id> conn;
+  conn.push_back(0);
+  conn.push_back(1);
+  conn.push_back(5);
+
+  conn.push_back(1);
+  conn.push_back(2);
+  conn.push_back(6);
+  conn.push_back(5);
+
+  conn.push_back(5);
+  conn.push_back(6);
+  conn.push_back(10);
+  conn.push_back(9);
+
+  conn.push_back(4);
+  conn.push_back(5);
+  conn.push_back(9);
+  conn.push_back(8);
+
+  conn.push_back(2);
+  conn.push_back(3);
+  conn.push_back(7);
+
+  conn.push_back(6);
+  conn.push_back(7);
+  conn.push_back(11);
+  conn.push_back(10);
+
+  conn.push_back(9);
+  conn.push_back(10);
+  conn.push_back(13);
+  conn.push_back(15);
+  conn.push_back(14);
+  conn.push_back(12);
+
+  static const vtkm::IdComponent ndim = 2;
+  vtkm::cont::CellSetExplicit<> cellSet(nVerts, "cells", ndim);
+  cellSet.FillViaCopy(shapes, numindices, conn);
+
+  dataSet.AddCellSet(cellSet);
+
+  return dataSet;
+}
+
+}
+
+//
+// Create an explicit 2D cell set as input and fill
+// Create an explicit 2D cell set as output
+// Points are all the same, but each cell becomes triangle cells
+//
+void TestExplicitGrid2D()
+{
+  std::cout << "Testing TriangulationExplicitGrid Filter" << std::endl;
+  typedef VTKM_DEFAULT_DEVICE_ADAPTER_TAG DeviceAdapter;
+
+  // Create the input uniform cell set
+  vtkm::cont::DataSet inDataSet = MakeTriangulateExplicitDataSet();
+  vtkm::cont::CellSetExplicit<> &inCellSet =
+      inDataSet.GetCellSet(0).CastTo<vtkm::cont::CellSetExplicit<> >();
+
+  vtkm::Id numberOfVertices = inCellSet.GetNumberOfPoints();
+
+  // Create the output dataset explicit cell set with same coordinate system
+  vtkm::cont::DataSet outDataSet;
+  vtkm::cont::CellSetExplicit<> outCellSet(numberOfVertices, "cells", 2);
+  outDataSet.AddCellSet(outCellSet);
+  outDataSet.AddCoordinateSystem(inDataSet.GetCoordinateSystem(0));
+
+  // Convert explicit cells to triangles
+  vtkm::worklet::TetrahedralizeFilterExplicitGrid<DeviceAdapter> 
+                 tetrahedralizeFilter(inDataSet, outDataSet);
+  tetrahedralizeFilter.Run();
+
+  vtkm::cont::CellSetExplicit<> cellSet = outDataSet.GetCellSet(0).CastTo<vtkm::cont::CellSetExplicit<> >();
+  vtkm::cont::CoordinateSystem coordinates = outDataSet.GetCoordinateSystem(0);
+  const vtkm::cont::DynamicArrayHandleCoordinateSystem coordArray = coordinates.GetData();
+  std::cout << "Number of output triangles " << cellSet.GetNumberOfCells() << std::endl;
+  std::cout << "Number of output vertices " << coordArray.GetNumberOfValues() << std::endl;
+  std::cout << "Number of output components " << coordArray.GetNumberOfComponents() << std::endl;
+
+  vtkm::cont::ArrayHandle<vtkm::Float64> bounds = coordinates.GetBounds(DeviceAdapter());
+  std::cout << "Bounds (" 
+            << bounds.GetPortalControl().Get(0) << "," << bounds.GetPortalControl().Get(1) << ") ("
+            << bounds.GetPortalControl().Get(2) << "," << bounds.GetPortalControl().Get(3) << ") ("
+            << bounds.GetPortalControl().Get(4) << "," << bounds.GetPortalControl().Get(5) << ")" << std::endl;
+
+  //VTKM_TEST_ASSERT(test_equal(outCellSet.GetNumberOfCells(), numberOfCells * 2),
+  //                 "Wrong result for Triangulate filter");
+}
+
+void TestTetrahedralizeExplicitGrid()
+{
+  TestExplicitGrid2D();
+}
+
+int UnitTestTetrahedralizeExplicitGrid(int, char *[])
+{
+  return vtkm::cont::testing::Testing::Run(TestTetrahedralizeExplicitGrid);
+}
