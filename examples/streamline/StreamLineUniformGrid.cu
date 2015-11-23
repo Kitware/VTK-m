34cc9719693c8b887448de15be58076f4714eba1
Patricia Kroll Fasel - 090207 <pkf@darwin-fe1.lanl.gov>
2015-11-23 12:50:13 -0700

    StreamLine particle tracing returns output dataset.

---
+++ examples/streamline/StreamLineUniformGrid.cu
@@ -0,0 +1,24 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2014 Sandia Corporation.
+//  Copyright 2014 UT-Battelle, LLC.
+//  Copyright 2014 Los Alamos National Security.
+//
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
+//============================================================================
+
+#define VTKM_DEVICE_ADAPTER VTKM_DEVICE_ADAPTER_CUDA
+#define BOOST_SP_DISABLE_THREADS
+
+#include "StreamLineUniformGrid.cxx"
