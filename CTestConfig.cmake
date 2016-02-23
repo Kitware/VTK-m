b507b4457efa4353c0af26979af1a9388119815c
Robert Maynard <robert.maynard@kitware.com>
2016-02-23 11:40:05 -0500

    Switch over to uploading by https as that is required by cdash.
    This will also verify that all the build machines have a version of cmake
    that has ssl enabled.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -27,7 +27,7 @@
 set(CTEST_PROJECT_NAME "VTKM")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "open.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=VTKM")
 set(CTEST_DROP_SITE_CDASH TRUE)
