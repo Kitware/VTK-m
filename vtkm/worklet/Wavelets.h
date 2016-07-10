c15633d66eef12a3e637b82c4c80d02a0772ece5
Samuel Li <samuelli@cs.uoregon.edu>
2016-07-10 13:46:40 -0600

    Filter class finished

--- vtkm/worklet/Wavelets.h
+++ vtkm/worklet/Wavelets.h
@@ -34,20 +34,28 @@ class Wavelets
 {
 public:
 
-	// helper class to hold a wavelet filter
+	// Wavelet filter class; 
+	// functionally equivalent to WaveFiltBase and its subclasses in VAPoR.
 	class Filter
 	{
   public:
 		// constructor
 		Filter( const std::string &wname )
 		{
+			lowDecomposeFilter = highDecomposeFilter = 
+				lowReconstructFilter = highReconstructFilter = NULL;
+		  this->filterLength = 0;
 			if( wname.compare("CDF9/7") == 0 )
 			{
 				this->filterLength = 9;
+				AllocateFilterMemory();
+				wrev( vtkm::worklet::internal::hm4_44,      lowDecomposeFilter, filterLength );
+				qmf_wrev( vtkm::worklet::internal::h4,      highDecomposeFilter, filterLength );
+				verbatim_copy( vtkm::worklet::internal::h4, lowReconstructFilter, filterLength );
+				qmf_even( vtkm::worklet::internal::hm4_44,  highReconstructFilter, filterLength );
 			}
 			else
 			{
-				this->filterLength = 0;
 				// throw an error here
 			}
 		}
@@ -55,34 +63,89 @@ public:
 		// destructor
 		virtual ~Filter()
 		{
-			if(  lowDecomposeFilter )		  delete[] lowDecomposeFilter;
-			if( highDecomposeFilter )		  delete[] lowDecomposeFilter;
-			if(  lowReconstructFilter )		delete[] lowDecomposeFilter;
-			if( highReconstructFilter )		delete[] lowDecomposeFilter;
+			if(  lowDecomposeFilter )		  	delete[] lowDecomposeFilter;
+			if(  highDecomposeFilter )		  delete[] highDecomposeFilter;
+			if(  lowReconstructFilter )			delete[] lowReconstructFilter; 
+			if(  highReconstructFilter )		delete[] highReconstructFilter;
 		}
 
 	protected:
 		vtkm::Id				 filterLength;
 		vtkm::Float64* 	 lowDecomposeFilter;
-		vtkm::Float64* 	highDecomposeFilter;
+		vtkm::Float64* 	 highDecomposeFilter;
 		vtkm::Float64*   lowReconstructFilter;
-		vtkm::Float64*	highReconstructFilter;
+		vtkm::Float64*	 highReconstructFilter;
 
 		void AllocateFilterMemory()
 		{
-			if( this->filterLength == 0 )
-				lowDecomposeFilter = highDecomposeFilter = 
-					lowReconstructFilter = highReconstructFilter = NULL;
-			else
+			lowDecomposeFilter    = new vtkm::Float64[ this->filterLength ];
+			highDecomposeFilter   = new vtkm::Float64[ this->filterLength ];
+			lowReconstructFilter  = new vtkm::Float64[ this->filterLength ];
+			highReconstructFilter = new vtkm::Float64[ this->filterLength ];
+		}
+		
+		// Flipping operation; helper function to initialize a filter.
+		void wrev( const vtkm::Float64* sigIn, vtkm::Float64* sigOut, vtkm::Id sigLength )
+		{
+			for( vtkm::Id count = 0; count < sigLength; count++)
+				sigOut[count] = sigIn[sigLength - count - 1];
+		}
+
+		// Quadrature mirror filtering operation: helper function to initialize a filter.
+		void qmf_even ( const vtkm::Float64* sigIn, vtkm::Float64* sigOut, vtkm::Id sigLength )
+		{
+			for (vtkm::Id count = 0; count < sigLength; count++) 
 			{
-				lowDecomposeFilter    = new vtkm::Float64[ this->filterLength ];
-				highDecomposeFilter   = new vtkm::Float64[ this->filterLength ];
-				lowReconstructFilter  = new vtkm::Float64[ this->filterLength ];
-				highReconstructFilter = new vtkm::Float64[ this->filterLength ];
+				sigOut[count] = sigIn[sigLength - count - 1];
+
+				if (sigLength % 2 == 0) {
+					if (count % 2 != 0) {
+						sigOut[count] = -1.0 * sigOut[count];
+					}
+				}
+				else {
+					if (count % 2 == 0) {
+						sigOut[count] = -1.0 * sigOut[count];
+					}
+				}
 			}
 		}
-	};
+		
+		// Flipping and QMF at the same time: helper function to initialize a filter.
+		void qmf_wrev ( const vtkm::Float64* sigIn, vtkm::Float64* sigOut, vtkm::Id sigLength )
+		{
+			for (vtkm::Id count = 0; count < sigLength; count++) {
+				sigOut[count] = sigIn[sigLength - count - 1];
 
+				if (sigLength % 2 == 0) {
+					if (count % 2 != 0) {
+						sigOut[count] = -1 * sigOut[count];
+					}
+				}
+				else {
+					if (count % 2 == 0) {
+						sigOut[count] = -1 * sigOut[count];
+					}
+				}
+			}
+
+			vtkm::Float64 tmp;
+			for (vtkm::Id count = 0; count < sigLength/2; count++) {
+				tmp = sigOut[count];
+				sigOut[count] = sigOut[sigLength - count - 1];
+				sigOut[sigLength - count - 1] = tmp;
+			}
+		}
+
+		// Verbatim Copying: helper function to initialize a filter.
+		void verbatim_copy ( const vtkm::Float64* sigIn, vtkm::Float64* sigOut, vtkm::Id sigLength )
+		{
+			for (vtkm::Id count = 0; count < sigLength; count++)
+				sigOut[count] = sigIn[count];
+		}
+	};	// class Filter
+
+	
   // helper worklet
   class ForwardTransform: public vtkm::worklet::WorkletMapField
   {
