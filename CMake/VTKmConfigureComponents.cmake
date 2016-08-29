696ca48bd1ec52d6b569368fe93c3d9e67f27c6f
Robert Maynard <robert.maynard@kitware.com>
2016-08-29 14:26:26 -0400

    Make sure CUDA is built with c++11 support enabled.

--- CMake/VTKmConfigureComponents.cmake
+++ CMake/VTKmConfigureComponents.cmake
@@ -223,7 +223,11 @@ macro(vtkm_configure_component_CUDA)
 
   if(VTKm_CUDA_FOUND)
     #---------------------------------------------------------------------------
-    # Setup build flags for CUDA
+    # Setup build flags for CUDA to have C++11 support
+    #---------------------------------------------------------------------------
+
+    list(APPEND CUDA_NVCC_FLAGS --std c++11)
+
     #---------------------------------------------------------------------------
     # Populates CUDA_NVCC_FLAGS with the best set of flags to compile for a
     # given GPU architecture. The majority of developers should leave the
