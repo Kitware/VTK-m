7a2874cf44c4db6528e4a505cf9d6704ff1e4aa7
Dave Pugmire <dpugmire@gmail.com>
2016-09-23 09:00:40 -0400

    Add 1D support for uniform and rectilinear data.

--- vtkm/rendering/testing/UnitTestMapperRayTracer.cxx
+++ vtkm/rendering/testing/UnitTestMapperRayTracer.cxx
@@ -67,7 +67,6 @@ void Render(const vtkm::cont::DataSet &ds,
                                           vtkm::rendering::ColorTable(ctName)));
 
     vtkm::rendering::View3D view(scene, mapper, canvas, camera, bg);
-
     view.Initialize();
     view.Paint();
     view.SaveAs(outputFile);
@@ -76,7 +75,6 @@ void Render(const vtkm::cont::DataSet &ds,
 void RenderTests()
 {
     vtkm::cont::testing::MakeTestDataSet maker;
-
     //3D tests.
     Render(maker.Make3DRegularDataSet0(),
              "pointvar", "thermal", "reg3D.pnm");
@@ -89,5 +87,5 @@ void RenderTests()
 } //namespace
 int UnitTestMapperRayTracer(int, char *[])
 {
-    return vtkm::cont::testing::Testing::Run(RenderTests);
+  return vtkm::cont::testing::Testing::Run(RenderTests);
 }
