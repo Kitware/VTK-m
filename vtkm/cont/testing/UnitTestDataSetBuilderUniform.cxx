e1c659a4fbd126784906d5b7f1e8d91ed45671e1
Dave Pugmire <dpugmire@gmail.com>
2016-11-03 15:15:40 -0400

    fix compiler warnings.

--- vtkm/cont/testing/UnitTestDataSetBuilderUniform.cxx
+++ vtkm/cont/testing/UnitTestDataSetBuilderUniform.cxx
@@ -188,9 +188,7 @@ UniformTests()
     numCells = dimensions[0]-1;
     bounds.Y = vtkm::Range(0, 0);
     bounds.Z = vtkm::Range(0, 0);
-    dataSet = dataSetBuilder.Create(dimensions[0],
-                                    static_cast<vtkm::FloatDefault>(origin[0]),
-                                    static_cast<vtkm::FloatDefault>(spacing[0]));
+    dataSet = dataSetBuilder.Create(dimensions[0], origin[0], spacing[0]);
     ValidateDataSet(dataSet, 1, numPoints, numCells, bounds);
   }
 }
