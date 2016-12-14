b85cb2dd3365e2817ccd18531c04682daa425241
Robert Maynard <robert.maynard@kitware.com>
2016-12-14 15:10:43 -0500

    Add a PointAverage Filter.

---
+++ vtkm/filter/PointAverage.hxx
@@ -0,0 +1,85 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2014 Sandia Corporation.
+//  Copyright 2014 UT-Battelle, LLC.
+//  Copyright 2014 Los Alamos National Security.
+//
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
+//============================================================================
+
+#include <vtkm/cont/DynamicCellSet.h>
+
+#include <vtkm/worklet/DispatcherMapTopology.h>
+
+namespace vtkm {
+namespace filter {
+
+
+//-----------------------------------------------------------------------------
+inline VTKM_CONT
+PointAverage::PointAverage():
+  vtkm::filter::FilterCell<PointAverage>(),
+  Worklet()
+{
+
+}
+
+//-----------------------------------------------------------------------------
+template<typename T,
+         typename StorageType,
+         typename DerivedPolicy,
+         typename DeviceAdapter>
+inline VTKM_CONT
+vtkm::filter::ResultField PointAverage::DoExecute(
+    const vtkm::cont::DataSet &input,
+    const vtkm::cont::ArrayHandle<T, StorageType> &inField,
+    const vtkm::filter::FieldMetadata &fieldMetadata,
+    const vtkm::filter::PolicyBase<DerivedPolicy>&,
+    const DeviceAdapter&)
+{
+  if(!fieldMetadata.IsCellField())
+  {
+    return vtkm::filter::ResultField();
+  }
+
+  vtkm::cont::DynamicCellSet cellSet =
+                  input.GetCellSet(this->GetActiveCellSetIndex());
+
+  //todo: we need to ask the policy what storage type we should be using
+  //If the input is implicit, we should know what to fall back to
+  vtkm::cont::ArrayHandle<T> outArray;
+
+  vtkm::worklet::DispatcherMapTopology<vtkm::worklet::PointAverage,
+                                       DeviceAdapter > dispatcher(this->Worklet);
+
+  //todo: we need to use the policy to determine the valid conversions
+  //that the dispatcher should do, including the result from GetCellSet
+  dispatcher.Invoke(cellSet, inField, outArray);
+
+  std::string outputName = this->GetOutputFieldName();
+  if (outputName.empty())
+  {
+    // Default name is name of input.
+    outputName = fieldMetadata.GetName();
+  }
+
+  return vtkm::filter::ResultField(input,
+                                   outArray,
+                                   outputName,
+                                   vtkm::cont::Field::ASSOC_POINTS,
+                                   cellSet.GetName());
+}
+
+}
+} // namespace vtkm::filter
