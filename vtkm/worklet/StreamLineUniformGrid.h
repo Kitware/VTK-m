c928b14cfea6a0dceea94eca9f51247dfe776af3
David C. Lonie <david.lonie@kitware.com>
2017-01-10 11:36:51 -0500

    Fix warnings on GCC6.

--- vtkm/worklet/StreamLineUniformGrid.h
+++ vtkm/worklet/StreamLineUniformGrid.h
@@ -252,9 +252,9 @@ public:
             pos[d] += (adata[d] + (2.0f * bdata[d]) + (2.0f * cdata[d]) + ddata[d]) / 6.0f;
           }
 
-          if (pos[0] < 0.0f || pos[0] > vdims[0] ||
-              pos[1] < 0.0f || pos[1] > vdims[1] ||
-              pos[2] < 0.0f || pos[2] > vdims[2])
+          if (pos[0] < 0.0f || pos[0] > static_cast<FieldType>(vdims[0]) ||
+              pos[1] < 0.0f || pos[1] > static_cast<FieldType>(vdims[1]) ||
+              pos[2] < 0.0f || pos[2] > static_cast<FieldType>(vdims[2]))
           {
             pos = pre_pos;
             done = true;
@@ -314,9 +314,9 @@ public:
             pos[d] += (adata[d] + (2.0f * bdata[d]) + (2.0f * cdata[d]) + ddata[d]) / 6.0f;
           }
 
-          if (pos[0] < 0.0f || pos[0] > vdims[0] ||
-              pos[1] < 0.0f || pos[1] > vdims[1] ||
-              pos[2] < 0.0f || pos[2] > vdims[2])
+          if (pos[0] < 0.0f || pos[0] > static_cast<FieldType>(vdims[0]) ||
+              pos[1] < 0.0f || pos[1] > static_cast<FieldType>(vdims[1]) ||
+              pos[2] < 0.0f || pos[2] > static_cast<FieldType>(vdims[2]))
           {
             pos = pre_pos;
             done = true;
