6fa2784db9f7a3ced774bfd5421a084c37e2e60c
Robert Maynard <robert.maynard@kitware.com>
2017-02-13 09:55:32 -0500

    Clanup some whitespace / newline issues in vtkm::worklet

--- vtkm/worklet/contourtree/Mesh2D_DEM_Triangulation.h
+++ vtkm/worklet/contourtree/Mesh2D_DEM_Triangulation.h
@@ -20,62 +20,62 @@
 //  Copyright (c) 2016, Los Alamos National Security, LLC
 //  All rights reserved.
 //
-//  Copyright 2016. Los Alamos National Security, LLC. 
-//  This software was produced under U.S. Government contract DE-AC52-06NA25396 
-//  for Los Alamos National Laboratory (LANL), which is operated by 
-//  Los Alamos National Security, LLC for the U.S. Department of Energy. 
-//  The U.S. Government has rights to use, reproduce, and distribute this 
-//  software.  NEITHER THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC 
-//  MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE 
-//  USE OF THIS SOFTWARE.  If software is modified to produce derivative works, 
-//  such modified software should be clearly marked, so as not to confuse it 
+//  Copyright 2016. Los Alamos National Security, LLC.
+//  This software was produced under U.S. Government contract DE-AC52-06NA25396
+//  for Los Alamos National Laboratory (LANL), which is operated by
+//  Los Alamos National Security, LLC for the U.S. Department of Energy.
+//  The U.S. Government has rights to use, reproduce, and distribute this
+//  software.  NEITHER THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC
+//  MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE
+//  USE OF THIS SOFTWARE.  If software is modified to produce derivative works,
+//  such modified software should be clearly marked, so as not to confuse it
 //  with the version available from LANL.
 //
-//  Additionally, redistribution and use in source and binary forms, with or 
-//  without modification, are permitted provided that the following conditions 
+//  Additionally, redistribution and use in source and binary forms, with or
+//  without modification, are permitted provided that the following conditions
 //  are met:
 //
-//  1. Redistributions of source code must retain the above copyright notice, 
+//  1. Redistributions of source code must retain the above copyright notice,
 //     this list of conditions and the following disclaimer.
 //  2. Redistributions in binary form must reproduce the above copyright notice,
 //     this list of conditions and the following disclaimer in the documentation
 //     and/or other materials provided with the distribution.
-//  3. Neither the name of Los Alamos National Security, LLC, Los Alamos 
-//     National Laboratory, LANL, the U.S. Government, nor the names of its 
-//     contributors may be used to endorse or promote products derived from 
+//  3. Neither the name of Los Alamos National Security, LLC, Los Alamos
+//     National Laboratory, LANL, the U.S. Government, nor the names of its
+//     contributors may be used to endorse or promote products derived from
 //     this software without specific prior written permission.
 //
-//  THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND 
-//  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-//  BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-//  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL LOS ALAMOS 
-//  NATIONAL SECURITY, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-//  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
-//  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
-//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+//  THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND
+//  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+//  BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL LOS ALAMOS
+//  NATIONAL SECURITY, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+//  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 //  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //============================================================================
 
-//  This code is based on the algorithm presented in the paper:  
-//  “Parallel Peak Pruning for Scalable SMP Contour Tree Computation.” 
-//  Hamish Carr, Gunther Weber, Christopher Sewell, and James Ahrens. 
-//  Proceedings of the IEEE Symposium on Large Data Analysis and Visualization 
+//  This code is based on the algorithm presented in the paper:
+//  “Parallel Peak Pruning for Scalable SMP Contour Tree Computation.”
+//  Hamish Carr, Gunther Weber, Christopher Sewell, and James Ahrens.
+//  Proceedings of the IEEE Symposium on Large Data Analysis and Visualization
 //  (LDAV), October 2016, Baltimore, Maryland.
 
 //=======================================================================================
 //
 // COMMENTS:
 //
-//	Essentially, a vector of data values. BUT we will want them sorted to simplify 
+//	Essentially, a vector of data values. BUT we will want them sorted to simplify
 //	processing - i.e. it's the robust way of handling simulation of simplicity
 //
 //	On the other hand, once we have them sorted, we can discard the original data since
 //	only the sort order matters
-// 
+//
 //	Since we've been running into memory issues, we'll start being more careful.
-//	Clearly, we can eliminate the values if we sort, but in this iteration we are 
+//	Clearly, we can eliminate the values if we sort, but in this iteration we are
 //	deferring doing a full sort, so we need to keep the values.
 //
 //=======================================================================================
@@ -116,7 +116,7 @@ public:
 
   // Array with neighbourhood masks
   vtkm::cont::ArrayHandle<vtkm::Id> neighbourhoodMask;
-	
+
   // constructor
   Mesh2D_DEM_Triangulation(const vtkm::cont::ArrayHandle<T,StorageType> &Values,
                            vtkm::Id NRows,
@@ -133,7 +133,7 @@ public:
 // sets outgoing paths for saddles
 template<typename T, typename StorageType, typename DeviceAdapter>
 void Mesh2D_DEM_Triangulation<T,StorageType,DeviceAdapter>::SetStarts(
-                                         vtkm::cont::ArrayHandle<vtkm::Id> &chains, 
+                                         vtkm::cont::ArrayHandle<vtkm::Id> &chains,
                                          bool ascending)
 {
   // create the neighbourhood mask
@@ -142,8 +142,8 @@ void Mesh2D_DEM_Triangulation<T,StorageType,DeviceAdapter>::SetStarts(
   // For each vertex set the next vertex in the chain
   vtkm::cont::ArrayHandleIndex vertexIndexArray(nVertices);
   Mesh2D_DEM_VertexStarter<T> vertexStarter(nRows, nCols, ascending);
-  vtkm::worklet::DispatcherMapField<Mesh2D_DEM_VertexStarter<T> > 
-                 vertexStarterDispatcher(vertexStarter); 
+  vtkm::worklet::DispatcherMapField<Mesh2D_DEM_VertexStarter<T> >
+                 vertexStarterDispatcher(vertexStarter);
 
   vertexStarterDispatcher.Invoke(vertexIndexArray,         // input
                                  values,                   // input (whole array)
@@ -162,7 +162,7 @@ Mesh2D_DEM_Triangulation<T,StorageType,DeviceAdapter>::Mesh2D_DEM_Triangulation(
                                nCols(NCols)
 {
   nVertices = nRows * nCols;
-  
+
   // compute the number of log-jumping steps (i.e. lg_2 (nVertices))
   nLogSteps = 1;
   for (vtkm::Id shifter = nVertices; shifter > 0; shifter >>= 1)
@@ -174,7 +174,7 @@ template<typename T, typename StorageType, typename DeviceAdapter>
 void Mesh2D_DEM_Triangulation<T,StorageType,DeviceAdapter>::SetSaddleStarts(ChainGraph<T,StorageType,DeviceAdapter> &mergeGraph,
                                                bool ascending)
 {
-  // we need a temporary inverse index to change vertex IDs 
+  // we need a temporary inverse index to change vertex IDs
   vtkm::cont::ArrayHandle<vtkm::Id> inverseIndex;
   vtkm::cont::ArrayHandle<vtkm::Id> isCritical;
   vtkm::cont::ArrayHandle<vtkm::Id> outdegree;
@@ -183,11 +183,11 @@ void Mesh2D_DEM_Triangulation<T,StorageType,DeviceAdapter>::SetSaddleStarts(Chai
   outdegree.Allocate(nVertices);
 
   vtkm::cont::ArrayHandleIndex vertexIndexArray(nVertices);
-  Mesh2D_DEM_VertexOutdegreeStarter vertexOutdegreeStarter(nRows, 
-                                                           nCols, 
+  Mesh2D_DEM_VertexOutdegreeStarter vertexOutdegreeStarter(nRows,
+                                                           nCols,
                                                            ascending);
-  vtkm::worklet::DispatcherMapField<Mesh2D_DEM_VertexOutdegreeStarter> 
-                 vertexOutdegreeStarterDispatcher(vertexOutdegreeStarter); 
+  vtkm::worklet::DispatcherMapField<Mesh2D_DEM_VertexOutdegreeStarter>
+                 vertexOutdegreeStarterDispatcher(vertexOutdegreeStarter);
 
   vertexOutdegreeStarterDispatcher.Invoke(vertexIndexArray,         // input
                                           neighbourhoodMask,        // input
@@ -196,14 +196,14 @@ void Mesh2D_DEM_Triangulation<T,StorageType,DeviceAdapter>::SetSaddleStarts(Chai
                                           isCritical);              // output
 
   DeviceAlgorithm::ScanExclusive(isCritical, inverseIndex);
-		
+
   // now we can compute how many critical points we carry forward
   vtkm::Id nCriticalPoints = inverseIndex.GetPortalConstControl().Get(nVertices-1) +
                              isCritical.GetPortalConstControl().Get(nVertices-1);
 
   // allocate space for the join graph vertex arrays
   mergeGraph.AllocateVertexArrays(nCriticalPoints);
-		
+
   // compact the set of vertex indices to critical ones only
   DeviceAlgorithm::StreamCompact(vertexIndexArray, isCritical, mergeGraph.valueIndex);
 
@@ -216,7 +216,7 @@ void Mesh2D_DEM_Triangulation<T,StorageType,DeviceAdapter>::SetSaddleStarts(Chai
   DeviceAlgorithm::StreamCompact(outdegree, isCritical, mergeGraph.outdegree);
 
   // copy the chain maximum from arcArray
-  // : mergeGraph.chainExtremum[vID] = inverseIndex[mergeGraph.arcArray[mergeGraph.valueIndex[vID]]]	
+  // : mergeGraph.chainExtremum[vID] = inverseIndex[mergeGraph.arcArray[mergeGraph.valueIndex[vID]]]
   typedef vtkm::cont::ArrayHandle<vtkm::Id> IdArrayType;
   typedef vtkm::cont::ArrayHandlePermutation<IdArrayType, IdArrayType> PermuteIndexType;
 
@@ -228,7 +228,7 @@ void Mesh2D_DEM_Triangulation<T,StorageType,DeviceAdapter>::SetSaddleStarts(Chai
   // and set up the active vertices - initially to identity
   vtkm::cont::ArrayHandleIndex criticalVertsIndexArray(nCriticalPoints);
   DeviceAlgorithm::Copy(criticalVertsIndexArray, mergeGraph.activeVertices);
-    
+
   // now we need to compute the firstEdge array from the outdegrees
   DeviceAlgorithm::ScanExclusive(mergeGraph.outdegree, mergeGraph.firstEdge);
 
@@ -242,8 +242,8 @@ void Mesh2D_DEM_Triangulation<T,StorageType,DeviceAdapter>::SetSaddleStarts(Chai
   Mesh2D_DEM_SaddleStarter saddleStarter(nRows,                     // input
                                          nCols,                     // input
                                          ascending);                // input
-  vtkm::worklet::DispatcherMapField<Mesh2D_DEM_SaddleStarter> 
-                 saddleStarterDispatcher(saddleStarter); 
+  vtkm::worklet::DispatcherMapField<Mesh2D_DEM_SaddleStarter>
+                 saddleStarterDispatcher(saddleStarter);
 
   vtkm::cont::ArrayHandleZip<vtkm::cont::ArrayHandle<vtkm::Id>, vtkm::cont::ArrayHandle<vtkm::Id> > outDegFirstEdge =
               vtkm::cont::make_ArrayHandleZip(mergeGraph.outdegree, mergeGraph.firstEdge);
