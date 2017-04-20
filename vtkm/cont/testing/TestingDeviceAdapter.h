ded4583aca857efba90bb297d381dd580518e104
Li-Ta Lo <lita.ollie@gmail.com>
2017-04-20 08:54:09 -0600

    attempt to fix warning on type conversion

--- vtkm/cont/testing/TestingDeviceAdapter.h
+++ vtkm/cont/testing/TestingDeviceAdapter.h
@@ -1365,7 +1365,7 @@ private:
 
     const vtkm::Id inputLength = ARRAY_SIZE;
     std::vector<vtkm::Id> inputKeys(inputLength);
-    for (int i = 0; i < ARRAY_SIZE; i++) {
+    for (vtkm::Id i = 0; i < ARRAY_SIZE; i++) {
       if (i % 100 < 98)
         inputKeys[i] = i / 100;
       else
@@ -1485,7 +1485,7 @@ private:
 
     const vtkm::Id inputLength = ARRAY_SIZE;
     std::vector<vtkm::Id> inputKeys(inputLength);
-    for (int i = 0; i < ARRAY_SIZE; i++) {
+    for (std::size_t i = 0; i < ARRAY_SIZE; i++) {
       if (i % 100 < 98)
         inputKeys[i] = i / 100;
       else
@@ -1496,7 +1496,7 @@ private:
 
     const vtkm::Id expectedLength = ARRAY_SIZE;
     std::vector<vtkm::Id> expectedValues(expectedLength);
-    for (auto i = 0; i < ARRAY_SIZE; i++) {
+    for (std::size_t i = 0; i < ARRAY_SIZE; i++) {
       if (i % 100 < 98)
         expectedValues[i] = init + i % 100;
       else
@@ -1512,7 +1512,7 @@ private:
 
     VTKM_TEST_ASSERT(valuesOut.GetNumberOfValues() == expectedLength,
                      "Got wrong number of output values");
-    for (auto i= 0; i < expectedLength; i++) {
+    for (std::size_t i= 0; i < expectedLength; i++) {
       const vtkm::Id v = valuesOut.GetPortalConstControl().Get(i);
       VTKM_TEST_ASSERT(expectedValues[i] == v, "Incorrect scanned value");
     }
@@ -1540,7 +1540,7 @@ private:
 
     VTKM_TEST_ASSERT(valuesOut.GetNumberOfValues() == expectedLength,
                      "Got wrong number of output values");
-    for (auto i= 0; i < expectedLength; i++) {
+    for (std::size_t i= 0; i < expectedLength; i++) {
       const vtkm::Id v = valuesOut.GetPortalConstControl().Get(i);
       VTKM_TEST_ASSERT(expectedValues[i] == v, "Incorrect scanned value");
     }
