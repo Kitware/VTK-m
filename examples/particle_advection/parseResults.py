f07509d71d75516fb36d1b8c3b5022da922aaf9d
Dave Pugmire <dpugmire@gmail.com>
2017-04-28 16:27:39 -0400

    Fixes for rhea

--- examples/particle_advection/parseResults.py
+++ examples/particle_advection/parseResults.py
@@ -5,8 +5,10 @@ def compare(x,y) :
     if x[0] == 'titan' :
         for i in reversed(range(16)) : A.append('TBB_%d' %(i+1))
     if x[0] == 'rheaGPU' :
-        for i in reversed(range(28)) : A.append('TBB_%d' %(i+1))
-            
+        for i in reversed(range(28*2)) : A.append('TBB_%d' %(i+1))
+    if x[0] == 'rhea' :
+        for i in reversed(range(16*2)) : A.append('TBB_%d' %(i+1))
+
     T = ['short','med','long']
     F = ['astro','fusion','fishtank']
     sx = (A.index(x[1]), F.index(x[2]), T.index(x[3]), x[4])
@@ -34,29 +36,34 @@ def printIt(x):
     for xi in x:
         print xi[0], xi[1], xi[2], xi[3], xi[4], xi[5]
 
-def printCompare(tD, rD) :
-    print 'FILE DIST SEEDS Titan-GPU Titan-TBB Rhea-GPU Rhea-TBB'
+def printCompare(tD, rcD, rgD) :
+    print 'FILE DIST SEEDS Titan-GPU Titan-TBB RheaG-GPU RheaG-TBB RheaC-TBB'
     for f in ['astro.bov', 'fusion.bov', 'fishtank.bov'] :
         for t in ['short', 'med', 'long'] :
             for s in [1000, 10000, 100000, 1000000, 10000000]:
-                t_gpu, r_gpu, t_tbb, r_tbb = (0,0,0,0)
+                t_gpu, rg_gpu, t_tbb, rg_tbb, rc_tbb = (0,0,0,0,0)
                 key = (f,'GPU',s,t)
                 if key in tD.keys() : t_gpu = tD[key]
-                if key in rD.keys() : r_gpu = rD[key]
+                if key in rgD.keys() : rg_gpu = rgD[key]
                 key = (f,'TBB_16', s,t)
                 if key in tD.keys() : t_tbb = tD[key]
                 key = (f,'TBB_28', s,t)
-                if key in rD.keys() : r_tbb = rD[key]
-                print f[:-4],t,s, t_gpu, t_tbb, r_gpu, r_tbb
+                if key in rgD.keys() : rg_tbb = rgD[key]
+                key = (f,'TBB_16', s,t)
+                if key in rcD.keys() : rc_tbb = rcD[key]
+                print f[:-4],t,s, t_gpu, t_tbb, rg_gpu, rg_tbb, rc_tbb
                     
 
 titanRaw = rawData('titan.pickle')
-rheaRaw = rawData('rheaGPU.pickle')
-printCompare(titanRaw, rheaRaw)
-    
+rheaCRaw = rawData('rhea.pickle')
+rheaGRaw = rawData('rheaGPU.pickle')
+printCompare(titanRaw, rheaCRaw, rheaGRaw)
+
 
 titanData = sorted(parseFile('titan.pickle'), cmp=compare)
-rheaData = sorted(parseFile('rheaGPU.pickle'), cmp=compare)
+rheaGData = sorted(parseFile('rheaGPU.pickle'), cmp=compare)
+rheaCData = sorted(parseFile('rhea.pickle'), cmp=compare)
 
 printIt(titanData)
-printIt(rheaData)
+printIt(rheaGData)
+printIt(rheaCData)
