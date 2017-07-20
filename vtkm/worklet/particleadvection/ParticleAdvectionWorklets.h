2aea25c88917fdf1c5244e62aa01f71c2d7b9ac2
Yenpure <abhishek.yenpure@hotmail.com>
2017-07-20 13:48:40 -0400

    Fix compile errors after merging Dave's changes
    -Changes in the Worklets class which did not reflect
     changes from the Particles class

--- vtkm/worklet/particleadvection/ParticleAdvectionWorklets.h
+++ vtkm/worklet/particleadvection/ParticleAdvectionWorklets.h
@@ -65,7 +65,7 @@ public:
       }
       else
       {
-        ic.SetStatusOutOfBounds(idx);
+        ic.SetExitedSpatialBoundary(idx);
       }
     }
   }
@@ -125,7 +125,7 @@ private:
     vtkm::Id numSeeds = static_cast<vtkm::Id>(seedArray.GetNumberOfValues());
 
     //Allocate status and steps arrays.
-    vtkm::cont::ArrayHandleConstant<vtkm::Id> ok(ParticleStatus::OK, numSeeds);
+    vtkm::cont::ArrayHandleConstant<vtkm::Id> ok(ParticleStatus::STATUS_OK, numSeeds);
     statusArray.Allocate(numSeeds);
     DeviceAlgorithm::Copy(ok, statusArray);
 
@@ -216,7 +216,7 @@ private:
         if (NeedParticleRounds && num > ParticlesPerRound)
           num = ParticlesPerRound;
 
-        std::vector<vtkm::Id> steps((size_t)num, 0), status((size_t)num, ParticleStatus::OK);
+        std::vector<vtkm::Id> steps((size_t)num, 0), status((size_t)num, ParticleStatus::STATUS_OK);
         vtkm::cont::ArrayHandle<vtkm::Id> stepArray = vtkm::cont::make_ArrayHandle(&steps[0], num);
         vtkm::cont::ArrayHandle<vtkm::Id> statusArray =
           vtkm::cont::make_ArrayHandle(&status[0], num);
@@ -256,7 +256,7 @@ private:
         if (NeedParticleRounds && num > ParticlesPerRound)
           num = ParticlesPerRound;
 
-        std::vector<vtkm::Id> steps((size_t)num, 0), status((size_t)num, ParticleStatus::OK);
+        std::vector<vtkm::Id> steps((size_t)num, 0), status((size_t)num, ParticleStatus::STATUS_OK);
         vtkm::cont::ArrayHandle<vtkm::Id> stepArray = vtkm::cont::make_ArrayHandle(&steps[0], num);
         vtkm::cont::ArrayHandle<vtkm::Id> statusArray =
           vtkm::cont::make_ArrayHandle(&status[0], num);
