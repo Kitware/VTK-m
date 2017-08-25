bc3b2e2bb598f1b9ebb7b9a6e0f31c663f1a6d79
dongliangchu <dc387@njit.edu>
2017-08-25 09:21:39 -0400

    reviewed version 1

--- vtkm/filter/FilterField.h
+++ vtkm/filter/FilterField.h
@@ -80,12 +80,6 @@ public:
     const vtkm::filter::PolicyBase<DerivedPolicy>& policy);
 
   template <typename DerivedPolicy>
-  VTKM_CONT std::vector<vtkm::filter::ResultField> Execute(
-    const vtkm::cont::MultiBlock& input,
-    const std::string& inFieldName,
-    const vtkm::filter::PolicyBase<DerivedPolicy>& policy);
-
-  template <typename DerivedPolicy>
   VTKM_CONT ResultField Execute(const vtkm::cont::DataSet& input,
                                 const std::string& inFieldName,
                                 const vtkm::filter::PolicyBase<DerivedPolicy>& policy);
