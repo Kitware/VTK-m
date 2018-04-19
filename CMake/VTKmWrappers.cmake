ffd98463f5faf2805ae18cec0d9014f5120ca6cd
Kenneth Moreland <kmorel@sandia.gov>
2018-04-19 15:14:32 -0600

    Load correct file extension in header test build
    Not all VTK-m headers end in .h, but the created test build files
    assumed that it was loading in a .h file. Fixed that.

--- CMake/VTKmWrappers.cmake
+++ CMake/VTKmWrappers.cmake
@@ -119,7 +119,7 @@ function(vtkm_add_header_build_test name dir_prefix use_cuda)
 //This is used by headers that include thrust to properly define a proper
 //device backend / system
 #define VTKM_TEST_HEADER_BUILD
-#include <${dir_prefix}/${headername}.h>
+#include <${dir_prefix}/${headername}.${headerextension}>
 int ${headername}_${headerextension}_testbuild_symbol;"
         )
       list(APPEND srcs ${src})
