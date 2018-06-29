b4110cd5dd5ba95b1f240c7a02ff01cd1a40ff83
Allison Vacanti <allison.vacanti@kitware.com>
2018-06-29 14:49:07 -0400

    Update update.sh for taotuple to reflect upstream changes.

--- vtkm/thirdparty/taotuple/update.sh
+++ vtkm/thirdparty/taotuple/update.sh
@@ -11,19 +11,12 @@ readonly repo="https://gitlab.kitware.com/third-party/$name.git"
 readonly tag="for/vtk-m"
 readonly paths="
 include
-git-subtree
 LICENSE
 README.md
 "
 
 extract_source () {
     git_archive
-    pushd "$extractdir/$name-reduced"
-    rm include/tao/seq
-    cp -r git-subtree/sequences/include/tao/seq include/tao/
-    rm -rf git-subtree
-    popd
-
 }
 
 . "${BASH_SOURCE%/*}/../update-common.sh"
