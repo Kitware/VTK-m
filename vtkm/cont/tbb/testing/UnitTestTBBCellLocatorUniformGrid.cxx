42e2bb7f9ab0402cf0013644fbf73f2785eb7d83
ayenpure <abhishek@uoregon.edu>
2018-08-29 19:46:49 -0700

    Updating files with copyrights

--- vtkm/cont/tbb/testing/UnitTestTBBCellLocatorUniformGrid.cxx
+++ vtkm/cont/tbb/testing/UnitTestTBBCellLocatorUniformGrid.cxx
@@ -6,9 +6,9 @@
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //  PURPOSE.  See the above copyright notice for more information.
 //
-//  Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
-//  Copyright 2017 UT-Battelle, LLC.
-//  Copyright 2017 Los Alamos National Security.
+//  Copyright 2018 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+//  Copyright 2018 UT-Battelle, LLC.
+//  Copyright 2018 Los Alamos National Security.
 //
 //  Under the terms of Contract DE-NA0003525 with NTESS,
 //  the U.S. Government retains certain rights in this software.
--- vtkm/cont/testing/TestingCellLocatorUniformGrid.h
+++ vtkm/cont/testing/TestingCellLocatorUniformGrid.h
@@ -6,9 +6,9 @@
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //  PURPOSE.  See the above copyright notice for more information.
 //
-//  Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
-//  Copyright 2014 UT-Battelle, LLC.
-//  Copyright 2017 Los Alamos National Security.
+//  Copyright 2018 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+//  Copyright 2018 UT-Battelle, LLC.
+//  Copyright 2018 Los Alamos National Security.
 //
 //  Under the terms of Contract DE-NA0003525 with NTESS,
 //  the U.S. Government retains certain rights in this software.
@@ -17,7 +17,6 @@
 //  Laboratory (LANL), the U.S. Government retains certain rights in
 //  this software.
 //============================================================================
-
 #ifndef vtk_m_cont_testing_TestingCellLocatorUniformGrid_h
 #define vtk_m_cont_testing_TestingCellLocatorUniformGrid_h
 
