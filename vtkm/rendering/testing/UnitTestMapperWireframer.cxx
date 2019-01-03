2f20549b4b342bd01d0dbdbbaac76b179dcbcdd2
Haocheng LIU <haocheng.liu@kitware.com>
2019-01-02 21:14:17 -0500

    Merge rendering testing executables to a shared library
    This commit allows rendering testing executables to select the device at runtime.

--- vtkm/rendering/testing/UnitTestMapperWireframer.cxx
+++ vtkm/rendering/testing/UnitTestMapperWireframer.cxx
@@ -139,8 +139,7 @@ void RenderTests()
 
 } //namespace
 
-int UnitTestMapperWireframer(int, char* [])
+int UnitTestMapperWireframer(int argc, char* argv[])
 {
-  vtkm::cont::GetGlobalRuntimeDeviceTracker().ForceDevice(VTKM_DEFAULT_DEVICE_ADAPTER_TAG());
-  return vtkm::cont::testing::Testing::Run(RenderTests);
+  return vtkm::cont::testing::Testing::Run(RenderTests, argc, argv);
 }
