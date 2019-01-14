887f79c6f406e56079d8930dac3888bdcc2d803c
Kenneth Moreland <kmorel@sandia.gov>
2019-01-14 14:50:08 -0700

    Make a vtkm_worklet library
    This is a library that contains parts of worklets that can be
    precompiled into a library.
    
    Currently, this library contains the implementation of ScatterCounting.

--- vtkm/worklet/ScatterCounting.h
+++ vtkm/worklet/ScatterCounting.h
@@ -22,22 +22,9 @@
 #ifndef vtk_m_worklet_ScatterCounting_h
 #define vtk_m_worklet_ScatterCounting_h
 
-#include <vtkm/cont/Algorithm.h>
-#include <vtkm/cont/ArrayCopy.h>
-#include <vtkm/cont/ArrayHandle.h>
-#include <vtkm/cont/ArrayHandleCast.h>
-#include <vtkm/cont/ArrayHandleConcatenate.h>
-#include <vtkm/cont/ArrayHandleConstant.h>
-#include <vtkm/cont/ArrayHandleIndex.h>
-#include <vtkm/cont/ArrayHandleView.h>
-#include <vtkm/cont/ErrorBadValue.h>
+#include <vtkm/worklet/vtkm_worklet_export.h>
 
-#include <vtkm/exec/FunctorBase.h>
-
-#include <vtkm/worklet/DispatcherMapField.h>
-#include <vtkm/worklet/WorkletMapField.h>
-
-#include <sstream>
+#include <vtkm/cont/VariantArrayHandle.h>
 
 namespace vtkm
 {
@@ -47,70 +34,7 @@ namespace worklet
 namespace detail
 {
 
-VTKM_CONT
-inline vtkm::cont::ArrayHandleConcatenate<
-  vtkm::cont::ArrayHandleConstant<vtkm::Id>,
-  vtkm::cont::ArrayHandleView<vtkm::cont::ArrayHandle<vtkm::Id>>>
-ShiftArrayHandleByOne(const vtkm::cont::ArrayHandle<vtkm::Id>& array)
-{
-  return vtkm::cont::make_ArrayHandleConcatenate(
-    vtkm::cont::make_ArrayHandleConstant<vtkm::Id>(0, 1),
-    vtkm::cont::make_ArrayHandleView(array, 0, array.GetNumberOfValues() - 1));
-}
-
-struct ReverseInputToOutputMapWorklet : vtkm::worklet::WorkletMapField
-{
-  using ControlSignature = void(FieldIn outputStartIndices,
-                                FieldIn outputEndIndices,
-                                WholeArrayOut outputToInputMap,
-                                WholeArrayOut visit);
-  using ExecutionSignature = void(_1, _2, _3, _4, InputIndex);
-  using InputDomain = _2;
-
-  template <typename OutputMapType, typename VisitType>
-  VTKM_EXEC void operator()(vtkm::Id outputStartIndex,
-                            vtkm::Id outputEndIndex,
-                            const OutputMapType& outputToInputMap,
-                            const VisitType& visit,
-                            vtkm::Id inputIndex) const
-  {
-    vtkm::IdComponent visitIndex = 0;
-    for (vtkm::Id outputIndex = outputStartIndex; outputIndex < outputEndIndex; outputIndex++)
-    {
-      outputToInputMap.Set(outputIndex, inputIndex);
-      visit.Set(outputIndex, visitIndex);
-      visitIndex++;
-    }
-  }
-
-  VTKM_CONT
-  static void Run(const vtkm::cont::ArrayHandle<vtkm::Id>& inputToOutputMap,
-                  const vtkm::cont::ArrayHandle<vtkm::Id>& outputToInputMap,
-                  const vtkm::cont::ArrayHandle<vtkm::IdComponent>& visit,
-                  vtkm::cont::DeviceAdapterId device)
-  {
-    vtkm::worklet::DispatcherMapField<ReverseInputToOutputMapWorklet> dispatcher;
-    dispatcher.SetDevice(device);
-    dispatcher.Invoke(
-      ShiftArrayHandleByOne(inputToOutputMap), inputToOutputMap, outputToInputMap, visit);
-  }
-};
-
-struct SubtractToVisitIndexWorklet : vtkm::worklet::WorkletMapField
-{
-  using ControlSignature = void(FieldIn startsOfGroup, WholeArrayOut visit);
-  using ExecutionSignature = void(InputIndex, _1, _2);
-  using InputDomain = _1;
-
-  template <typename VisitType>
-  VTKM_EXEC void operator()(vtkm::Id inputIndex,
-                            vtkm::Id startOfGroup,
-                            const VisitType& visit) const
-  {
-    vtkm::IdComponent visitIndex = static_cast<vtkm::IdComponent>(inputIndex - startOfGroup);
-    visit.Set(inputIndex, visitIndex);
-  }
-};
+struct ScatterCountingBuilder;
 
 } // namespace detail
 
@@ -126,8 +50,20 @@ struct SubtractToVisitIndexWorklet : vtkm::worklet::WorkletMapField
 /// taken in the constructor and the index arrays are derived from that. So
 /// changing the counts after the scatter is created will have no effect.
 ///
-struct ScatterCounting
+struct VTKM_WORKLET_EXPORT ScatterCounting
 {
+  struct CountTypes : vtkm::ListTagBase<vtkm::Int64,
+                                        vtkm::Int32,
+                                        vtkm::Int16,
+                                        vtkm::Int8,
+                                        vtkm::UInt64,
+                                        vtkm::UInt32,
+                                        vtkm::UInt16,
+                                        vtkm::UInt8>
+  {
+  };
+  using VariantArrayHandleCount = vtkm::cont::VariantArrayHandleBase<CountTypes>;
+
   /// Construct a \c ScatterCounting object using an array of counts for the
   /// number of outputs for each input. Part of the construction requires
   /// generating an input to output map, but this map is not needed for the
@@ -135,15 +71,27 @@ struct ScatterCounting
   /// other users might make use of it, so you can instruct the constructor
   /// to save the input to output map.
   ///
-  template <typename CountArrayType>
-  VTKM_CONT ScatterCounting(const CountArrayType& countArray,
+  template <typename TypeList>
+  VTKM_CONT ScatterCounting(const vtkm::cont::VariantArrayHandleBase<TypeList>& countArray,
+                            vtkm::cont::DeviceAdapterId device = vtkm::cont::DeviceAdapterTagAny(),
+                            bool saveInputToOutputMap = false)
+  {
+    this->BuildArrays(VariantArrayHandleCount(countArray), device, saveInputToOutputMap);
+  }
+  VTKM_CONT ScatterCounting(const VariantArrayHandleCount& countArray,
                             vtkm::cont::DeviceAdapterId device = vtkm::cont::DeviceAdapterTagAny(),
                             bool saveInputToOutputMap = false)
   {
     this->BuildArrays(countArray, device, saveInputToOutputMap);
   }
-  template <typename CountArrayType>
-  VTKM_CONT ScatterCounting(const CountArrayType& countArray, bool saveInputToOutputMap)
+  template <typename TypeList>
+  VTKM_CONT ScatterCounting(const vtkm::cont::VariantArrayHandleBase<TypeList>& countArray,
+                            bool saveInputToOutputMap)
+  {
+    this->BuildArrays(
+      VariantArrayHandleCount(countArray), vtkm::cont::DeviceAdapterTagAny(), saveInputToOutputMap);
+  }
+  VTKM_CONT ScatterCounting(const VariantArrayHandleCount& countArray, bool saveInputToOutputMap)
   {
     this->BuildArrays(countArray, vtkm::cont::DeviceAdapterTagAny(), saveInputToOutputMap);
   }
@@ -196,83 +144,11 @@ private:
   OutputToInputMapType OutputToInputMap;
   VisitArrayType VisitArray;
 
-  template <typename CountArrayType>
-  VTKM_CONT void BuildArrays(const CountArrayType& count,
+  friend struct detail::ScatterCountingBuilder;
+
+  VTKM_CONT void BuildArrays(const VariantArrayHandleCount& countArray,
                              vtkm::cont::DeviceAdapterId device,
-                             bool saveInputToOutputMap)
-  {
-    VTKM_IS_ARRAY_HANDLE(CountArrayType);
-
-    this->InputRange = count.GetNumberOfValues();
-
-    // The input to output map is actually built off by one. The first entry
-    // is actually for the second value. The last entry is the total number of
-    // output. This off-by-one is so that an upper bound find will work when
-    // building the output to input map. Later we will either correct the
-    // map or delete it.
-    vtkm::cont::ArrayHandle<vtkm::Id> inputToOutputMapOffByOne;
-    vtkm::Id outputSize = vtkm::cont::Algorithm::ScanInclusive(
-      device, vtkm::cont::make_ArrayHandleCast(count, vtkm::Id()), inputToOutputMapOffByOne);
-
-    // We have implemented two different ways to compute the output to input
-    // map. The first way is to use a binary search on each output index into
-    // the input map. The second way is to schedule on each input and
-    // iteratively fill all the output indices for that input. The first way is
-    // faster for output sizes that are small relative to the input (typical in
-    // Marching Cubes, for example) and also tends to be well load balanced.
-    // The second way is faster for larger outputs (typical in triangulation,
-    // for example). We will use the first method for small output sizes and
-    // the second for large output sizes. Toying with this might be a good
-    // place for optimization.
-    if (outputSize < this->InputRange)
-    {
-      this->BuildOutputToInputMapWithFind(outputSize, device, inputToOutputMapOffByOne);
-    }
-    else
-    {
-      this->BuildOutputToInputMapWithIterate(outputSize, device, inputToOutputMapOffByOne);
-    }
-
-    if (saveInputToOutputMap)
-    {
-      // Since we are saving it, correct the input to output map.
-      vtkm::cont::Algorithm::Copy(
-        device, detail::ShiftArrayHandleByOne(inputToOutputMapOffByOne), this->InputToOutputMap);
-    }
-  }
-
-  VTKM_CONT void BuildOutputToInputMapWithFind(
-    vtkm::Id outputSize,
-    vtkm::cont::DeviceAdapterId device,
-    vtkm::cont::ArrayHandle<vtkm::Id> inputToOutputMapOffByOne)
-  {
-    vtkm::cont::ArrayHandleIndex outputIndices(outputSize);
-    vtkm::cont::Algorithm::UpperBounds(
-      device, inputToOutputMapOffByOne, outputIndices, this->OutputToInputMap);
-
-    vtkm::cont::ArrayHandle<vtkm::Id> startsOfGroups;
-
-    // This find gives the index of the start of a group.
-    vtkm::cont::Algorithm::LowerBounds(
-      device, this->OutputToInputMap, this->OutputToInputMap, startsOfGroups);
-
-    this->VisitArray.Allocate(outputSize);
-    vtkm::worklet::DispatcherMapField<detail::SubtractToVisitIndexWorklet> dispatcher;
-    dispatcher.SetDevice(device);
-    dispatcher.Invoke(startsOfGroups, this->VisitArray);
-  }
-
-  VTKM_CONT void BuildOutputToInputMapWithIterate(
-    vtkm::Id outputSize,
-    vtkm::cont::DeviceAdapterId device,
-    vtkm::cont::ArrayHandle<vtkm::Id> inputToOutputMapOffByOne)
-  {
-    this->OutputToInputMap.Allocate(outputSize);
-    this->VisitArray.Allocate(outputSize);
-
-    detail::ReverseInputToOutputMapWorklet::Run(
-      inputToOutputMapOffByOne, this->OutputToInputMap, this->VisitArray, device);
-  }
+                             bool saveInputToOutputMap);
 };
 }
 } // namespace vtkm::worklet
