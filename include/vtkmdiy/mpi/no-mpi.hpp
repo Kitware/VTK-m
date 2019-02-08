0adcc4d45695b01c6d16bc12600ef45da724fd5c
Diy Upstream <kwrobot@kitware.com>
2019-02-08 14:10:57 -0500

    diy 2019-02-08 (72a201e1)
    Code extracted from:
    
        https://gitlab.kitware.com/third-party/diy2.git
    
    at commit 72a201e1d5f8e35c9e28b66ce9faa34dd5365680 (for/vtk-m).

--- include/vtkmdiy/mpi/no-mpi.hpp
+++ include/vtkmdiy/mpi/no-mpi.hpp
@@ -1,5 +1,5 @@
-#ifndef DIY_MPI_NO_MPI_HPP
-#define DIY_MPI_NO_MPI_HPP
+#ifndef VTKMDIY_MPI_NO_MPI_HPP
+#define VTKMDIY_MPI_NO_MPI_HPP
 
 #include <stdexcept> // std::runtime_error
 
@@ -22,17 +22,17 @@ static const int MPI_THREAD_MULTIPLE    = 3;
 /* define datatypes */
 using MPI_Datatype = size_t;
 
-#define DIY_NO_MPI_DATATYPE(cpp_type, mpi_type) \
+#define VTKM_DIY_NO_MPI_DATATYPE(cpp_type, mpi_type) \
   static const MPI_Datatype mpi_type = sizeof(cpp_type);
-DIY_NO_MPI_DATATYPE(char,                  MPI_BYTE);
-DIY_NO_MPI_DATATYPE(int,                   MPI_INT);
-DIY_NO_MPI_DATATYPE(unsigned,              MPI_UNSIGNED);
-DIY_NO_MPI_DATATYPE(long,                  MPI_LONG);
-DIY_NO_MPI_DATATYPE(unsigned long,         MPI_UNSIGNED_LONG);
-DIY_NO_MPI_DATATYPE(long long,             MPI_LONG_LONG_INT);
-DIY_NO_MPI_DATATYPE(unsigned long long,    MPI_UNSIGNED_LONG_LONG);
-DIY_NO_MPI_DATATYPE(float,                 MPI_FLOAT);
-DIY_NO_MPI_DATATYPE(double,                MPI_DOUBLE);
+VTKM_DIY_NO_MPI_DATATYPE(char,                  MPI_BYTE);
+VTKM_DIY_NO_MPI_DATATYPE(int,                   MPI_INT);
+VTKM_DIY_NO_MPI_DATATYPE(unsigned,              MPI_UNSIGNED);
+VTKM_DIY_NO_MPI_DATATYPE(long,                  MPI_LONG);
+VTKM_DIY_NO_MPI_DATATYPE(unsigned long,         MPI_UNSIGNED_LONG);
+VTKM_DIY_NO_MPI_DATATYPE(long long,             MPI_LONG_LONG_INT);
+VTKM_DIY_NO_MPI_DATATYPE(unsigned long long,    MPI_UNSIGNED_LONG_LONG);
+VTKM_DIY_NO_MPI_DATATYPE(float,                 MPI_FLOAT);
+VTKM_DIY_NO_MPI_DATATYPE(double,                MPI_DOUBLE);
 #endif
 
 /* status type */
@@ -48,8 +48,10 @@ struct MPI_Status
 /* define MPI_Request */
 using MPI_Request = int;
 
+#ifndef DIY_UNSUPPORTED_MPI_CALL
 #define DIY_UNSUPPORTED_MPI_CALL(name) \
-  throw std::runtime_error("`" #name "` not supported when DIY_NO_MPI is defined.");
+  throw std::runtime_error("`" #name "` not supported when VTKM_DIY_NO_MPI is defined.");
+#endif
 
 /* define operations */
 using MPI_Op = int;
