f78e763be4666118b5b3c14d77761b2d7dce2e15
Sujin Philip <sujin.philip@kitware.com>
2019-03-29 13:28:27 -0400

    Add CellLocatorGeneral
    A general purpose `CellLocator` that should work with any type of dataset.
    Internally, it tries to chose one of the existing cell locators that would be
    optimal for the input data.
    
    Also removes `CellLocatorHelper`.

---
+++ vtkm/cont/CellLocatorGeneral.cxx
@@ -0,0 +1,103 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+//  Copyright 2019 UT-Battelle, LLC.
+//  Copyright 2019 Los Alamos National Security.
+//
+//  Under the terms of Contract DE-NA0003525 with NTESS,
+//  the U.S. Government retains certain rights in this software.
+//
+//  Under the terms of Contract DE-AC52-06NA25396 with Los Alamos National
+//  Laboratory (LANL), the U.S. Government retains certain rights in
+//  this software.
+//============================================================================
+#include <vtkm/cont/CellLocatorGeneral.h>
+
+#include <vtkm/cont/ArrayHandle.h>
+#include <vtkm/cont/ArrayHandleCartesianProduct.h>
+#include <vtkm/cont/ArrayHandleUniformPointCoordinates.h>
+#include <vtkm/cont/CellLocatorRectilinearGrid.h>
+#include <vtkm/cont/CellLocatorUniformBins.h>
+#include <vtkm/cont/CellLocatorUniformGrid.h>
+#include <vtkm/cont/CellSetStructured.h>
+
+namespace
+{
+
+VTKM_CONT
+void DefaultConfigurator(std::unique_ptr<vtkm::cont::CellLocator>& locator,
+                         const vtkm::cont::DynamicCellSet& cellSet,
+                         const vtkm::cont::CoordinateSystem& coords)
+{
+  using StructuredCellSet = vtkm::cont::CellSetStructured<3>;
+  using UniformCoordinates = vtkm::cont::ArrayHandleUniformPointCoordinates;
+  using RectilinearCoordinates =
+    vtkm::cont::ArrayHandleCartesianProduct<vtkm::cont::ArrayHandle<vtkm::FloatDefault>,
+                                            vtkm::cont::ArrayHandle<vtkm::FloatDefault>,
+                                            vtkm::cont::ArrayHandle<vtkm::FloatDefault>>;
+
+  if (cellSet.IsType<StructuredCellSet>() && coords.GetData().IsType<UniformCoordinates>())
+  {
+    if (!dynamic_cast<vtkm::cont::CellLocatorUniformGrid*>(locator.get()))
+    {
+      locator.reset(new vtkm::cont::CellLocatorUniformGrid);
+    }
+  }
+  else if (cellSet.IsType<StructuredCellSet>() && coords.GetData().IsType<RectilinearCoordinates>())
+  {
+    if (!dynamic_cast<vtkm::cont::CellLocatorRectilinearGrid*>(locator.get()))
+    {
+      locator.reset(new vtkm::cont::CellLocatorRectilinearGrid);
+    }
+  }
+  else if (!dynamic_cast<vtkm::cont::CellLocatorUniformBins*>(locator.get()))
+  {
+    locator.reset(new vtkm::cont::CellLocatorUniformBins);
+  }
+
+  locator->SetCellSet(cellSet);
+  locator->SetCoordinates(coords);
+}
+
+} // anonymous namespace
+
+namespace vtkm
+{
+namespace cont
+{
+
+VTKM_CONT CellLocatorGeneral::CellLocatorGeneral()
+  : Configurator(DefaultConfigurator)
+{
+}
+
+VTKM_CONT CellLocatorGeneral::~CellLocatorGeneral() = default;
+
+VTKM_CONT const vtkm::exec::CellLocator* CellLocatorGeneral::PrepareForExecution(
+  vtkm::cont::DeviceAdapterId device) const
+{
+  if (this->Locator)
+  {
+    return this->Locator->PrepareForExecution(device);
+  }
+  return nullptr;
+}
+
+VTKM_CONT void CellLocatorGeneral::Build()
+{
+  this->Configurator(this->Locator, this->GetCellSet(), this->GetCoordinates());
+  this->Locator->Update();
+}
+
+VTKM_CONT void CellLocatorGeneral::ResetToDefaultConfigurator()
+{
+  this->SetConfigurator(DefaultConfigurator);
+}
+}
+} // vtkm::cont
