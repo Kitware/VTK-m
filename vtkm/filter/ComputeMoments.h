f64721c2bc444d1206e87f098a05b7fc032f367b
Li-Ta Lo <ollie@lanl.gov>
2019-04-23 16:14:56 -0600

    update copyright notice

--- vtkm/filter/ComputeMoments.h
+++ vtkm/filter/ComputeMoments.h
@@ -1,7 +1,12 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
 //
-// Created by ollie on 4/18/19.
-//
-
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=======================================================================
 #ifndef vtk_m_filter_ComputeMoments_h
 #define vtk_m_filter_ComputeMoments_h
 
