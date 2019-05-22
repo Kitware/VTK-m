b9d109ab3d27d6f2c06904c68edb19d961fb937e
Dave Pugmire <dpugmire@gmail.com>
2019-05-22 13:08:12 -0400

    Fix for CellEdgeFace test. Case is identical to polygon.

--- vtkm/exec/testing/UnitTestCellEdgeFace.cxx
+++ vtkm/exec/testing/UnitTestCellEdgeFace.cxx
@@ -215,7 +215,13 @@ struct TestCellFacesFunctor
     this->TryShapeWithNumPoints(vtkm::CellTraits<CellShapeTag>::NUM_POINTS, CellShapeTag());
   }
 
-  void operator()(vtkm::CellShapeTagPolyLine) const { throw "POLYLINE not supported yet"; }
+  void operator()(vtkm::CellShapeTagPolyLine) const
+  {
+    for (vtkm::IdComponent numPoints = 3; numPoints < 7; numPoints++)
+    {
+      this->TryShapeWithNumPoints(numPoints, vtkm::CellShapeTagPolygon());
+    }
+  }
 
   void operator()(vtkm::CellShapeTagPolygon) const
   {
