dc90a838e0be84f7ae2cf4db8f1edb1e16063efa
Abhishek Yenpure <abhishek@uoregon.edu>
2019-08-01 15:31:55 -0600

    Updating copyright statement for Integrators.h

--- vtkm/worklet/particleadvection/Integrators.h
+++ vtkm/worklet/particleadvection/Integrators.h
@@ -1,12 +1,14 @@
-//============================================================================
-//  Copyrigth (c) Kitware, Inc.
-//  All rigths reserved.
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
 //  See LICENSE.txt for details.
 //
 //  This software is distributed WITHOUT ANY WARRANTY; without even
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-//  PURPOSE.  See the above copyrigth notice for more information.
-//============================================================================
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//=============================================================================
 
 #ifndef vtk_m_worklet_particleadvection_Integrators_h
 #define vtk_m_worklet_particleadvection_Integrators_h
