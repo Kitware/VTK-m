5bd6046b95f7b5a17cd24f2399d3f1eb22fd171c
Abhishek Yenpure <abhishek@uoregon.edu>
2019-08-05 13:15:10 -0700

    Adding LCS example

---
+++ examples/lagrangian_structures/LagrangianStructures.cxx
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <string>
+#include <vector>
+
+#include <vtkm/cont/ArrayCopy.h>
+#include <vtkm/cont/DataSet.h>
+#include <vtkm/cont/DataSetBuilderUniform.h>
+#include <vtkm/cont/DataSetFieldAdd.h>
+#include <vtkm/io/reader/VTKDataSetReader.h>
+#include <vtkm/io/writer/VTKDataSetWriter.h>
+
+#include <vtkm/filter/LagrangianStructures.h>
+
+int main(int argc, char** argv)
+{
+  if (argc < 3)
+  {
+    std::cout << "Usage : flte <input dataset> <vector field name>" << std::endl;
+  }
+  std::string datasetName(argv[1]);
+  std::string variableName(argv[2]);
+
+  std::cout << "Reading input dataset" << std::endl;
+  vtkm::cont::DataSet input;
+  vtkm::io::reader::VTKDataSetReader reader(datasetName);
+  input = reader.ReadDataSet();
+  std::cout << "Read input dataset" << std::endl;
+
+  vtkm::filter::LagrangianStructures lcsFilter;
+  lcsFilter.SetStepSize(0.025);
+  lcsFilter.SetNumberOfSteps(500);
+  lcsFilter.SetAdvectionTime(0.025 * 500);
+  lcsFilter.SetActiveField(variableName);
+
+  vtkm::cont::DataSet output = lcsFilter.Execute(input);
+  vtkm::io::writer::VTKDataSetWriter writer("out.vtk");
+  writer.WriteDataSet(output);
+  std::cout << "Written output dataset" << std::endl;
+
+  return 0;
+}
