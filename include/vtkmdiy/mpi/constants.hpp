aa68517c22795d32bae25999d657a8fa88362cbd
Diy Upstream <kwrobot@kitware.com>
2019-08-22 09:18:39 -0400

    diy 2019-08-22 (2153469e)
    Code extracted from:
    
        https://gitlab.kitware.com/third-party/diy2.git
    
    at commit 2153469e21736806f766e59e73aff5ad790b9dfa (for/vtk-m).

---
+++ include/vtkmdiy/mpi/constants.hpp
@@ -0,0 +1,13 @@
+#ifndef VTKMDIY_MPI_CONSTANTS_HPP
+#define VTKMDIY_MPI_CONSTANTS_HPP
+
+namespace diy
+{
+namespace mpi
+{
+  const int any_source  = MPI_ANY_SOURCE;
+  const int any_tag     = MPI_ANY_TAG;
+}
+}
+
+#endif
