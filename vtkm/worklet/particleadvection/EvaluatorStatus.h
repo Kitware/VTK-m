001617b2ad0326a3c375c5b8bc7ecd1c1b2fbe31
Dan Lipsa <dan.lipsa@kitware.com>
2019-08-27 08:41:50 -0400

    Fix comment and install.

--- vtkm/worklet/particleadvection/EvaluatorStatus.h
+++ vtkm/worklet/particleadvection/EvaluatorStatus.h
@@ -1,3 +1,15 @@
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//=============================================================================
+
 #ifndef vtk_m_worklet_particleadvection_EvaluatorStatus_h
 #define vtk_m_worklet_particleadvection_EvaluatorStatus_h
 
