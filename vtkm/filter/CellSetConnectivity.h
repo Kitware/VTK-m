25fb666df90e49735fab1b3ee18970c347374e28
Robert Maynard <robert.maynard@kitware.com>
2019-09-26 13:49:54 -0400

    CellSetConnectivity now inherits from FilterDataSet
    Fixes #409

--- vtkm/filter/CellSetConnectivity.h
+++ vtkm/filter/CellSetConnectivity.h
@@ -11,23 +11,46 @@
 #ifndef vtkm_m_filter_CellSetConnectivity_h
 #define vtkm_m_filter_CellSetConnectivity_h
 
-#include <vtkm/filter/FilterCell.h>
+#include <vtkm/filter/FilterDataSet.h>
 
 namespace vtkm
 {
 namespace filter
 {
-class CellSetConnectivity : public vtkm::filter::FilterCell<CellSetConnectivity>
+/// \brief Finds groups of cells that are connected together through their topology.
+///
+/// Finds and labels groups of cells that are connected together through their topology.
+/// Two cells are considered connected if they share an edge. CellSetConnectivity identifies some
+/// number of components and assigns each component a unique integer.
+/// The result of the filter is a cell field of type vtkm::Id with the default name of 'component'.
+/// Each entry in the cell field will be a number that identifies to which component the cell belongs.
+class CellSetConnectivity : public vtkm::filter::FilterDataSet<CellSetConnectivity>
 {
 public:
   using SupportedTypes = vtkm::TypeListTagScalarAll;
   VTKM_CONT CellSetConnectivity();
 
-  template <typename T, typename StorageType, typename DerivedPolicy>
+  void SetOutputFieldName(const std::string& name) { this->OutputFieldName = name; }
+
+  VTKM_CONT
+  const std::string& GetOutputFieldName() const { return this->OutputFieldName; }
+
+
+  template <typename Policy>
   VTKM_CONT vtkm::cont::DataSet DoExecute(const vtkm::cont::DataSet& input,
-                                          const vtkm::cont::ArrayHandle<T, StorageType>& field,
-                                          const vtkm::filter::FieldMetadata& fieldMetadata,
-                                          const vtkm::filter::PolicyBase<DerivedPolicy>&);
+                                          vtkm::filter::PolicyBase<Policy> policy);
+
+  template <typename Policy>
+  VTKM_CONT bool MapFieldOntoOutput(vtkm::cont::DataSet& result,
+                                    const vtkm::cont::Field& field,
+                                    const vtkm::filter::PolicyBase<Policy>&)
+  {
+    result.AddField(field);
+    return true;
+  }
+
+private:
+  std::string OutputFieldName;
 };
 }
 }
