445e4d8186a2fbfa403d5a6d4df165af421492cb
Robert Maynard <robert.maynard@kitware.com>
2019-09-27 16:40:53 -0400

    vtkm::cont::Invoker supports both Masks and Scatter
    Fixes #420

--- vtkm/worklet/MaskNone.h
+++ vtkm/worklet/MaskNone.h
@@ -11,6 +11,7 @@
 #define vtk_m_worklet_MaskNone_h
 
 #include <vtkm/cont/ArrayHandleIndex.h>
+#include <vtkm/worklet/internal/MaskBase.h>
 
 namespace vtkm
 {
@@ -23,7 +24,7 @@ namespace worklet
 /// domain. This is the default mask object so that the worklet is run for every possible
 /// output element.
 ///
-struct MaskNone
+struct MaskNone : public internal::MaskBase
 {
   template <typename RangeType>
   VTKM_CONT RangeType GetThreadRange(RangeType outputRange) const
--- vtkm/worklet/internal/DecayHelpers.h
+++ vtkm/worklet/internal/DecayHelpers.h
@@ -7,11 +7,10 @@
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //  PURPOSE.  See the above copyright notice for more information.
 //============================================================================
-#ifndef vtk_m_worklet_internal_ScatterBase_h
-#define vtk_m_worklet_internal_ScatterBase_h
+#ifndef vtk_m_worklet_internal_DecayHelpers_h
+#define vtk_m_worklet_internal_DecayHelpers_h
 
-#include <vtkm/internal/ExportMacros.h>
-#include <vtkm/worklet/internal/DecayHelpers.h>
+#include <type_traits>
 
 namespace vtkm
 {
@@ -19,17 +18,12 @@ namespace worklet
 {
 namespace internal
 {
-/// Base class for all scatter classes.
-///
-/// This allows VTK-m to determine when a parameter
-/// is a scatter type instead of a worklet parameter.
-///
-struct VTKM_ALWAYS_EXPORT ScatterBase
-{
-};
 
 template <typename T>
-using is_scatter = std::is_base_of<ScatterBase, remove_cvref<T>>;
+using remove_pointer_and_decay = typename std::remove_pointer<typename std::decay<T>::type>::type;
+
+template <typename T>
+using remove_cvref = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
 }
 }
 }
