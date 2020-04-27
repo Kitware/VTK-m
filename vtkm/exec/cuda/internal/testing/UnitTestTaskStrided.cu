a44392027ae8f2e587b0d8e3f195c25c428fc96d
Kenneth Moreland <kmorel@sandia.gov>
2020-04-27 17:47:37 -0600

    Fix compile error in UnitTestTaskStrided.cu from changes in Fetch
    A recent change removed the thread indices parameters from the arguments
    to the `Fetch` template. Somehow, an instance of using the old template
    in the CUDA task strided tests snuck through the dashboard tests.
    Correct that.

--- vtkm/exec/cuda/internal/testing/UnitTestTaskStrided.cu
+++ vtkm/exec/cuda/internal/testing/UnitTestTaskStrided.cu
@@ -242,7 +242,6 @@ VTKM_STATIC_ASSERT(
                   InvocationToFetch<vtkm::exec::arg::ThreadIndicesBasic, InvocationType1, 1>::type,
                 vtkm::exec::arg::Fetch<TestFetchTagInput,
                                        vtkm::exec::arg::AspectTagDefault,
-                                       vtkm::exec::arg::ThreadIndicesBasic,
                                        TestExecObject>>::type::value));
 
 VTKM_STATIC_ASSERT(
@@ -250,7 +249,6 @@ VTKM_STATIC_ASSERT(
                   InvocationToFetch<vtkm::exec::arg::ThreadIndicesBasic, InvocationType1, 2>::type,
                 vtkm::exec::arg::Fetch<TestFetchTagOutput,
                                        vtkm::exec::arg::AspectTagDefault,
-                                       vtkm::exec::arg::ThreadIndicesBasic,
                                        TestExecObject>>::type::value));
 
 VTKM_STATIC_ASSERT(
@@ -258,7 +256,6 @@ VTKM_STATIC_ASSERT(
                   InvocationToFetch<vtkm::exec::arg::ThreadIndicesBasic, InvocationType2, 0>::type,
                 vtkm::exec::arg::Fetch<TestFetchTagOutput,
                                        vtkm::exec::arg::AspectTagDefault,
-                                       vtkm::exec::arg::ThreadIndicesBasic,
                                        TestExecObject>>::type::value));
 
 template <typename DeviceAdapter>
