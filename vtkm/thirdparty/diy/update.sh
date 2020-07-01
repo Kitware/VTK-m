5a2a26d3fc3e7a8b9707e63bef0b69e643b0d28c
Robert Maynard <robert.maynard@kitware.com>
2020-07-01 10:48:18 -0400

    Update DIY to make sure we always build diy libs with pic enabled

--- vtkm/thirdparty/diy/update.sh
+++ vtkm/thirdparty/diy/update.sh
@@ -8,7 +8,7 @@ readonly name="diy"
 readonly ownership="Diy Upstream <kwrobot@kitware.com>"
 readonly subtree="vtkm/thirdparty/$name/vtkm$name"
 readonly repo="https://gitlab.kitware.com/third-party/diy2.git"
-readonly tag="for/vtk-m-20200626-master"
+readonly tag="for/vtk-m-20200701-master"
 readonly paths="
 cmake
 include
