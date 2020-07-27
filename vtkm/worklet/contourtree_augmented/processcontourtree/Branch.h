cd43328ef71dc753dbc5fb7286924eb8f6dddd56
Petar Hristov <mm16pgh@leeds.ac.uk>
2020-07-27 10:43:20 +0100

    Fixed signed conversion build warning.

--- vtkm/worklet/contourtree_augmented/processcontourtree/Branch.h
+++ vtkm/worklet/contourtree_augmented/processcontourtree/Branch.h
@@ -158,7 +158,7 @@ Branch<T>* Branch<T>::ComputeBranchDecomposition(
   const IdArrayType& sortOrder,
   const vtkm::cont::ArrayHandle<T, StorageType>& dataField,
   bool dataFieldIsSorted)
-{ // ComputeBranchDecomposition()
+{ // C)omputeBranchDecomposition()
   auto branchMinimumPortal = branchMinimum.ReadPortal();
   auto branchMaximumPortal = branchMaximum.ReadPortal();
   auto branchSaddlePortal = branchSaddle.ReadPortal();
@@ -177,7 +177,7 @@ Branch<T>* Branch<T>::ComputeBranchDecomposition(
   // Reconstruct explicit branch decomposition from array representation
   for (std::size_t branchID = 0; branchID < static_cast<std::size_t>(nBranches); ++branchID)
   {
-    branches[branchID]->OriginalId = branchID;
+    branches[branchID]->OriginalId = static_cast<vtkm::Id>(branchID);
     if (!NoSuchElement(branchSaddlePortal.Get(static_cast<vtkm::Id>(branchID))))
     {
       branches[branchID]->Saddle = MaskedIndex(
