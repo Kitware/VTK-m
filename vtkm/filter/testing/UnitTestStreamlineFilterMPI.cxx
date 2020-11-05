b57ffb30ae412eb8a7e092bd98e044a772f16ae8
Dave Pugmire <dpugmire@gmail.com>
2020-11-05 15:34:11 -0500

    Add parallel AMR streamline test.

--- vtkm/filter/testing/UnitTestStreamlineFilterMPI.cxx
+++ vtkm/filter/testing/UnitTestStreamlineFilterMPI.cxx
@@ -36,6 +36,191 @@ void AddVectorFields(vtkm::cont::PartitionedDataSet& pds,
     ds.AddPointField(fieldName, CreateConstantVectorField(ds.GetNumberOfPoints(), vec));
 }
 
+void TestAMRStreamline(bool useSL)
+{
+  auto comm = vtkm::cont::EnvironmentTracker::GetCommunicator();
+  if (comm.size() < 2)
+    return;
+
+  vtkm::Bounds outerBounds(0, 10, 0, 10, 0, 10);
+  vtkm::Id3 outerDims(11, 11, 11);
+  auto outerDataSets = vtkm::worklet::testing::CreateAllDataSets(outerBounds, outerDims, false);
+
+  vtkm::Bounds innerBounds(3.8, 5.2, 3.8, 5.2, 3.8, 5.2);
+  vtkm::Bounds innerBoundsNoGhost(4, 5, 4, 5, 4, 5);
+  vtkm::Id3 innerDims(12, 12, 12);
+  auto innerDataSets = vtkm::worklet::testing::CreateAllDataSets(innerBounds, innerDims, true);
+
+  std::size_t numDS = outerDataSets.size();
+  for (std::size_t d = 0; d < numDS; d++)
+  {
+    auto dsOuter = outerDataSets[d];
+    auto dsInner = innerDataSets[d];
+
+    //Add ghost cells for the outerDataSets.
+    //One interior cell is a ghost.
+    std::vector<vtkm::UInt8> ghosts;
+    ghosts.resize(dsOuter.GetCellSet().GetNumberOfCells());
+    vtkm::Id idx = 0;
+    for (vtkm::Id i = 0; i < outerDims[0] - 1; i++)
+      for (vtkm::Id j = 0; j < outerDims[1] - 1; j++)
+        for (vtkm::Id k = 0; k < outerDims[2] - 1; k++)
+        {
+          //Mark the inner cell as ghost.
+          if (i == 4 && j == 4 && k == 4)
+            ghosts[idx] = vtkm::CellClassification::GHOST;
+          else
+            ghosts[idx] = vtkm::CellClassification::NORMAL;
+          idx++;
+        }
+    dsOuter.AddCellField("vtkmGhostCells", ghosts);
+
+    //Create a partitioned dataset with 1 inner and 1 outer.
+    vtkm::cont::PartitionedDataSet pds;
+    if (comm.rank() == 0)
+      pds.AppendPartition(dsOuter);
+    else if (comm.rank() == 1)
+      pds.AppendPartition(dsInner);
+
+    std::string fieldName = "vec";
+    vtkm::Vec3f vecX(1, 0, 0);
+    AddVectorFields(pds, fieldName, vecX);
+
+    //seed 0 goes right through the center of the inner
+    vtkm::Particle p0(vtkm::Vec3f(static_cast<vtkm::FloatDefault>(1),
+                                  static_cast<vtkm::FloatDefault>(4.5),
+                                  static_cast<vtkm::FloatDefault>(4.5)),
+                      0);
+
+    //seed 1 goes remains entirely in the outer
+    vtkm::Particle p1(vtkm::Vec3f(static_cast<vtkm::FloatDefault>(1),
+                                  static_cast<vtkm::FloatDefault>(3),
+                                  static_cast<vtkm::FloatDefault>(3)),
+                      1);
+
+    vtkm::cont::ArrayHandle<vtkm::Particle> seedArray;
+    seedArray = vtkm::cont::make_ArrayHandle({ p0, p1 });
+    vtkm::Id numSeeds = seedArray.GetNumberOfValues();
+
+    if (useSL)
+    {
+      vtkm::filter::Streamline filter;
+      filter.SetStepSize(0.1f);
+      filter.SetNumberOfSteps(100000);
+      filter.SetSeeds(seedArray);
+
+      filter.SetActiveField(fieldName);
+      auto out = filter.Execute(pds);
+
+      if (comm.rank() <= 1)
+        VTKM_TEST_ASSERT(out.GetNumberOfPartitions() == 1, "Wrong number of partitions in output");
+      else
+        continue;
+
+      auto ds = out.GetPartition(0);
+
+      //validate the outer (rank 0)
+      if (comm.rank() == 0)
+      {
+        VTKM_TEST_ASSERT(ds.GetNumberOfPoints() == 173, "Wrong number of coordinates");
+        VTKM_TEST_ASSERT(ds.GetNumberOfCoordinateSystems() == 1,
+                         "Wrong number of coordinate systems in the output dataset");
+        auto coords = ds.GetCoordinateSystem().GetDataAsMultiplexer();
+        auto ptPortal = coords.ReadPortal();
+        vtkm::cont::DynamicCellSet dcells = ds.GetCellSet();
+
+        VTKM_TEST_ASSERT(dcells.IsType<vtkm::cont::CellSetExplicit<>>(), "Wrong cell type.");
+        //The seed that goes through the inner is broken up into two polylines
+        //the begining, and then the end.
+        VTKM_TEST_ASSERT(dcells.GetNumberOfCells() == numSeeds + 1, "Wrong number of cells.");
+        auto explicitCells = dcells.Cast<vtkm::cont::CellSetExplicit<>>();
+        for (vtkm::Id j = 0; j < numSeeds; j++)
+        {
+          vtkm::cont::ArrayHandle<vtkm::Id> indices;
+          explicitCells.GetIndices(j, indices);
+          vtkm::Id nPts = indices.GetNumberOfValues();
+          auto iPortal = indices.ReadPortal();
+          vtkm::Vec3f lastPt = ptPortal.Get(iPortal.Get(nPts - 1));
+
+          if (j == 0) //this is the seed that goes THROUGH inner.
+          {
+            VTKM_TEST_ASSERT(outerBounds.Contains(lastPt),
+                             "End point is NOT inside the outer bounds.");
+            VTKM_TEST_ASSERT(innerBounds.Contains(lastPt),
+                             "End point is NOT inside the inner bounds.");
+          }
+          else
+          {
+            VTKM_TEST_ASSERT(!outerBounds.Contains(lastPt),
+                             "Seed final location is INSIDE the dataset");
+            VTKM_TEST_ASSERT(lastPt[0] > outerBounds.X.Max,
+                             "Seed final location in wrong location");
+          }
+        }
+      }
+
+      //validate the inner (rank 1)
+      else if (comm.rank() == 1)
+      {
+        VTKM_TEST_ASSERT(ds.GetNumberOfPoints() == 12, "Wrong number of coordinates");
+        VTKM_TEST_ASSERT(ds.GetNumberOfCoordinateSystems() == 1,
+                         "Wrong number of coordinate systems in the output dataset");
+        auto coords = ds.GetCoordinateSystem().GetDataAsMultiplexer();
+        auto ptPortal = coords.ReadPortal();
+        auto dcells = ds.GetCellSet();
+
+        VTKM_TEST_ASSERT(dcells.IsType<vtkm::cont::CellSetExplicit<>>(), "Wrong cell type.");
+        VTKM_TEST_ASSERT(dcells.GetNumberOfCells() == 1, "Wrong number of cells.");
+        auto explicitCells = dcells.Cast<vtkm::cont::CellSetExplicit<>>();
+
+        vtkm::cont::ArrayHandle<vtkm::Id> indices;
+        explicitCells.GetIndices(0, indices);
+        vtkm::Id nPts = indices.GetNumberOfValues();
+        auto iPortal = indices.ReadPortal();
+        vtkm::Vec3f lastPt = ptPortal.Get(iPortal.Get(nPts - 1));
+
+        //The last point should be OUTSIDE innerBoundsNoGhost but inside innerBounds
+        VTKM_TEST_ASSERT(!innerBoundsNoGhost.Contains(lastPt) && innerBounds.Contains(lastPt),
+                         "Seed final location not contained in bounds correctly.");
+        VTKM_TEST_ASSERT(lastPt[0] > innerBoundsNoGhost.X.Max,
+                         "Seed final location in wrong location");
+      }
+    }
+    else
+    {
+      vtkm::filter::ParticleAdvection filter;
+      filter.SetStepSize(0.1f);
+      filter.SetNumberOfSteps(100000);
+      filter.SetSeeds(seedArray);
+
+      filter.SetActiveField(fieldName);
+      auto out = filter.Execute(pds);
+
+      if (comm.rank() == 0)
+      {
+        VTKM_TEST_ASSERT(out.GetNumberOfPartitions() == 1, "Wrong number of partitions in output");
+        auto ds = out.GetPartition(0);
+        VTKM_TEST_ASSERT(ds.GetNumberOfCoordinateSystems() == 1,
+                         "Wrong number of coordinate systems in the output dataset");
+        vtkm::cont::DynamicCellSet dcells = ds.GetCellSet();
+        VTKM_TEST_ASSERT(dcells.IsType<vtkm::cont::CellSetSingleType<>>(), "Wrong cell type.");
+
+        auto coords = ds.GetCoordinateSystem().GetDataAsMultiplexer();
+        auto ptPortal = coords.ReadPortal();
+        VTKM_TEST_ASSERT(ds.GetNumberOfPoints() == numSeeds, "Wrong number of coordinates");
+
+        for (vtkm::Id i = 0; i < numSeeds; i++)
+        {
+          VTKM_TEST_ASSERT(!outerBounds.Contains(ptPortal.Get(i)),
+                           "Seed final location is INSIDE the dataset");
+          VTKM_TEST_ASSERT(ptPortal.Get(i)[0] > outerBounds.X.Max,
+                           "Seed final location in wrong location");
+        }
+      }
+    }
+  }
+}
+
 void TestPartitionedDataSet(vtkm::Id nPerRank, bool useGhost, bool useSL)
 {
   auto comm = vtkm::cont::EnvironmentTracker::GetCommunicator();
@@ -175,6 +360,9 @@ void TestStreamlineFiltersMPI()
       for (auto useSL : flags)
         TestPartitionedDataSet(n, useGhost, useSL);
   }
+
+  for (auto useSL : flags)
+    TestAMRStreamline(useSL);
 }
 }
 
