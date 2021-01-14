ff0b8fd0bb82c7405eff30aa9f73e2d672c1136f
Li-Ta Lo <ollie@lanl.gov>
2021-01-14 14:23:05 -0700

    changed to CMake files based on Robert's feedback

--- CMake/testing/VTKmCheckSourceInInstall.cmake
+++ CMake/testing/VTKmCheckSourceInInstall.cmake
@@ -18,6 +18,7 @@
 #        -DVTKm_INSTALL_INCLUDE_DIR=<VTKm_INSTALL_INCLUDE_DIR>
 #        -DVTKm_ENABLE_RENDERING=<VTKm_ENABLE_RENDERING>
 #        -DVTKm_ENABLE_LOGGING=<VTKm_ENABLE_LOGGING>
+#        -DVTKm_ENABLE_HDF5_IO=<VTKm_ENABLE_HDF5_IO>
 #        -P <VTKm_SOURCE_DIR>/CMake/testing/VTKMCheckSourceInInstall.cmake
 ##
 
@@ -39,7 +40,9 @@ endif ()
 if (NOT DEFINED VTKm_ENABLE_LOGGING)
   message(FATAL_ERROR "VTKm_ENABLE_LOGGING not defined.")
 endif ()
-
+if (NOT DEFINED VTKm_ENABLE_HDF5_IO)
+  message(FATAL_ERROR "VTKm_ENABLE_HDF5_IO not defined.")
+endif()
 
 include(CMakeParseArguments)
 # -----------------------------------------------------------------------------
@@ -121,13 +124,6 @@ function(do_verify root_dir prefix)
     cont/StorageVirtual.hxx
     )
 
-  if (NOT VTKm_ENABLE_HDF5_IO)
-    set(file_exceptions ${file_exceptions}
-      io/ImageWriterHDF5.h
-      io/ImageReaderHDF5.h
-      )
-  endif()
-
   #by default every header in a testing directory doesn't need to be installed
   set(directory_exceptions ".*/testing" )
   # These exceptions should be based on the status of the associated
@@ -138,7 +134,12 @@ function(do_verify root_dir prefix)
   if(NOT VTKm_ENABLE_LOGGING)
     list(APPEND directory_exceptions thirdparty/loguru)
   endif()
-
+  if (NOT VTKm_ENABLE_HDF5_IO)
+    list(APPEND file_exceptions
+      io/ImageWriterHDF5.h
+      io/ImageReaderHDF5.h
+      )
+  endif()
   #Step 2. Verify the installed files match what headers are listed in each
   # source directory
   verify_install_per_dir("${VTKm_SOURCE_DIR}/vtkm"
