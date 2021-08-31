0e6228bbd8dd60e5cc07cfa8c7012f4f696704dd
James <james@jameskress.com>
2021-08-31 15:33:45 -0400

    Updating threshold to return all or part in range.

--- vtkm/filter/testing/UnitTestThresholdFilter.cxx
+++ vtkm/filter/testing/UnitTestThresholdFilter.cxx
@@ -10,7 +10,6 @@
 
 #include <vtkm/cont/testing/MakeTestDataSet.h>
 #include <vtkm/cont/testing/Testing.h>
-
 #include <vtkm/filter/CleanGrid.h>
 #include <vtkm/filter/Threshold.h>
 
@@ -22,28 +21,55 @@ namespace
 class TestingThreshold
 {
 public:
-  void TestRegular2D() const
+  void TestRegular2D(bool returnAllInRange) const
   {
-    std::cout << "Testing threshold on 2D regular dataset" << std::endl;
-
     vtkm::cont::DataSet dataset = MakeTestDataSet().Make2DUniformDataSet0();
-
     vtkm::filter::Threshold threshold;
-    threshold.SetLowerThreshold(60.1);
-    threshold.SetUpperThreshold(60.1);
+
+    if (returnAllInRange)
+    {
+      std::cout << "Testing threshold on 2D regular dataset returning values 'all in range'"
+                << std::endl;
+      threshold.SetLowerThreshold(10);
+      threshold.SetUpperThreshold(60);
+    }
+    else
+    {
+      std::cout << "Testing threshold on 2D regular dataset returning values 'part in range'"
+                << std::endl;
+      threshold.SetLowerThreshold(60);
+      threshold.SetUpperThreshold(61);
+    }
+
+    threshold.SetAllInRange(returnAllInRange);
     threshold.SetActiveField("pointvar");
     threshold.SetFieldsToPass("cellvar");
     auto output = threshold.Execute(dataset);
 
-    VTKM_TEST_ASSERT(output.GetNumberOfFields() == 1,
-                     "Wrong number of fields in the output dataset");
+    if (returnAllInRange)
+    {
+      VTKM_TEST_ASSERT(output.GetNumberOfFields() == 1,
+                       "Wrong number of fields in the output dataset");
 
-    vtkm::cont::ArrayHandle<vtkm::Float32> cellFieldArray;
-    output.GetField("cellvar").GetData().AsArrayHandle(cellFieldArray);
+      vtkm::cont::ArrayHandle<vtkm::Float32> cellFieldArray;
+      output.GetField("cellvar").GetData().AsArrayHandle(cellFieldArray);
 
-    VTKM_TEST_ASSERT(cellFieldArray.GetNumberOfValues() == 1 &&
-                       cellFieldArray.ReadPortal().Get(0) == 200.1f,
-                     "Wrong cell field data");
+      VTKM_TEST_ASSERT(cellFieldArray.GetNumberOfValues() == 1 &&
+                         cellFieldArray.ReadPortal().Get(0) == 100.1f,
+                       "Wrong cell field data");
+    }
+    else
+    {
+      VTKM_TEST_ASSERT(output.GetNumberOfFields() == 1,
+                       "Wrong number of fields in the output dataset");
+
+      vtkm::cont::ArrayHandle<vtkm::Float32> cellFieldArray;
+      output.GetField("cellvar").GetData().AsArrayHandle(cellFieldArray);
+
+      VTKM_TEST_ASSERT(cellFieldArray.GetNumberOfValues() == 1 &&
+                         cellFieldArray.ReadPortal().Get(0) == 200.1f,
+                       "Wrong cell field data");
+    }
 
     // Make sure that the resulting data set can be successfully passed to another
     // simple filter using the cell set.
@@ -51,29 +77,58 @@ public:
     clean.Execute(output);
   }
 
-  void TestRegular3D() const
+  void TestRegular3D(bool returnAllInRange) const
   {
-    std::cout << "Testing threshold on 3D regular dataset" << std::endl;
     vtkm::cont::DataSet dataset = MakeTestDataSet().Make3DUniformDataSet0();
-
     vtkm::filter::Threshold threshold;
 
-    threshold.SetLowerThreshold(20.1);
-    threshold.SetUpperThreshold(20.1);
+    if (returnAllInRange)
+    {
+      std::cout << "Testing threshold on 3D regular dataset returning values 'all in range'"
+                << std::endl;
+      threshold.SetLowerThreshold(10.1);
+      threshold.SetUpperThreshold(180);
+    }
+    else
+    {
+      std::cout << "Testing threshold on 3D regular dataset returning values 'part in range'"
+                << std::endl;
+      threshold.SetLowerThreshold(20);
+      threshold.SetUpperThreshold(21);
+    }
+
+    threshold.SetAllInRange(returnAllInRange);
     threshold.SetActiveField("pointvar");
     threshold.SetFieldsToPass("cellvar");
     auto output = threshold.Execute(dataset);
 
-    VTKM_TEST_ASSERT(output.GetNumberOfFields() == 1,
-                     "Wrong number of fields in the output dataset");
+    if (returnAllInRange)
+    {
+      VTKM_TEST_ASSERT(output.GetNumberOfFields() == 1,
+                       "Wrong number of fields in the output dataset");
 
-    vtkm::cont::ArrayHandle<vtkm::Float32> cellFieldArray;
-    output.GetField("cellvar").GetData().AsArrayHandle(cellFieldArray);
+      vtkm::cont::ArrayHandle<vtkm::Float32> cellFieldArray;
+      output.GetField("cellvar").GetData().AsArrayHandle(cellFieldArray);
 
-    VTKM_TEST_ASSERT(cellFieldArray.GetNumberOfValues() == 2 &&
-                       cellFieldArray.ReadPortal().Get(0) == 100.1f &&
-                       cellFieldArray.ReadPortal().Get(1) == 100.2f,
-                     "Wrong cell field data");
+      VTKM_TEST_ASSERT(cellFieldArray.GetNumberOfValues() == 3 &&
+                         cellFieldArray.ReadPortal().Get(0) == 100.1f &&
+                         cellFieldArray.ReadPortal().Get(1) == 100.2f &&
+                         cellFieldArray.ReadPortal().Get(2) == 100.3f,
+                       "Wrong cell field data");
+    }
+    else
+    {
+      VTKM_TEST_ASSERT(output.GetNumberOfFields() == 1,
+                       "Wrong number of fields in the output dataset");
+
+      vtkm::cont::ArrayHandle<vtkm::Float32> cellFieldArray;
+      output.GetField("cellvar").GetData().AsArrayHandle(cellFieldArray);
+
+      VTKM_TEST_ASSERT(cellFieldArray.GetNumberOfValues() == 2 &&
+                         cellFieldArray.ReadPortal().Get(0) == 100.1f &&
+                         cellFieldArray.ReadPortal().Get(1) == 100.2f,
+                       "Wrong cell field data");
+    }
 
     // Make sure that the resulting data set can be successfully passed to another
     // simple filter using the cell set.
@@ -88,8 +143,8 @@ public:
 
     vtkm::filter::Threshold threshold;
 
-    threshold.SetLowerThreshold(20.1);
-    threshold.SetUpperThreshold(20.1);
+    threshold.SetLowerThreshold(20);
+    threshold.SetUpperThreshold(21);
     threshold.SetActiveField("pointvar");
     threshold.SetFieldsToPass("cellvar");
     auto output = threshold.Execute(dataset);
@@ -118,7 +173,7 @@ public:
 
     vtkm::filter::Threshold threshold;
 
-    threshold.SetLowerThreshold(500.1);
+    threshold.SetLowerThreshold(500);
     threshold.SetUpperThreshold(500.1);
     threshold.SetActiveField("pointvar");
     threshold.SetFieldsToPass("cellvar");
@@ -140,8 +195,10 @@ public:
 
   void operator()() const
   {
-    this->TestRegular2D();
-    this->TestRegular3D();
+    this->TestRegular2D(false);
+    this->TestRegular2D(true);
+    this->TestRegular3D(false);
+    this->TestRegular3D(true);
     this->TestExplicit3D();
     this->TestExplicit3DZeroResults();
   }
