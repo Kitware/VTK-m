7139a1c8c43c432932ff9d7e5bb09b1b6dc498d4
Roxana Bujack <bujack@lanl.gov>
2021-12-13 09:33:47 -0700

    add amr capability as filter

--- vtkm/CellClassification.h
+++ vtkm/CellClassification.h
@@ -19,7 +19,7 @@ enum CellClassification : vtkm::UInt8
   GHOST = 1 << 0,   //Ghost cell
   INVALID = 1 << 1, //Cell is invalid
   UNUSED0 = 1 << 2,
-  UNUSED1 = 1 << 3,
+  BLANKED = 1 << 3, //Blanked cell in AMR
   UNUSED3 = 1 << 4,
   UNUSED4 = 1 << 5,
   UNUSED5 = 1 << 6,
