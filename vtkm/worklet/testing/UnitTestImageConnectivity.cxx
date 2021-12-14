942653bbe2f20a9f1e61e5831e3d1c9cff72c77d
Li-Ta Lo <ollie@lanl.gov>
2021-12-13 19:11:40 -0700

    Extract implementation of MakeTestDataSet to .cxx

--- vtkm/worklet/testing/UnitTestImageConnectivity.cxx
+++ vtkm/worklet/testing/UnitTestImageConnectivity.cxx
@@ -7,7 +7,7 @@
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //  PURPOSE.  See the above copyright notice for more information.
 //============================================================================
-#include <vtkm/cont/testing/MakeTestDataSet.h>
+#include <vtkm/cont/DataSetBuilderUniform.h>
 #include <vtkm/cont/testing/Testing.h>
 #include <vtkm/filter/Contour.h>
 
