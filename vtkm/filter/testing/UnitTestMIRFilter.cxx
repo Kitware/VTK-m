ed65758a5e07e5d94f175d467c4a830ce9ff1447
Abhishek Yenpure <abhishek@uoregon.edu>
2022-01-13 18:25:39 -0800

    Adding MIR unit test with data (Venn from Ascent examples)

--- vtkm/filter/testing/UnitTestMIRFilter.cxx
+++ vtkm/filter/testing/UnitTestMIRFilter.cxx
@@ -9,9 +9,12 @@
 //============================================================================
 
 #include <vtkm/cont/DataSetBuilderExplicit.h>
+#include <vtkm/cont/Invoker.h>
 #include <vtkm/cont/testing/Testing.h>
 
 #include <vtkm/filter/MIRFilter.h>
+
+#include <vtkm/io/VTKDataSetReader.h>
 
 void ConnectionHelperHex(std::vector<vtkm::Id>& conn, int x, int y, int z, int mx, int my, int mz)
 {
@@ -92,7 +95,147 @@ vtkm::cont::DataSet GetTestDataSet()
   return ds;
 }
 
-void TestMIR()
+class MetaDataLength : public vtkm::worklet::WorkletMapField
+{
+public:
+  using ControlSignature = void(FieldIn, FieldIn, FieldIn, FieldIn, FieldOut);
+
+  void operator()(const vtkm::FloatDefault& background,
+                  const vtkm::FloatDefault& circle_a,
+                  const vtkm::FloatDefault& circle_b,
+                  const vtkm::FloatDefault& circle_c,
+                  vtkm::Id& length) const
+  {
+    length = 0;
+    if (background > 0)
+      length++;
+    if (circle_a > 0)
+      length++;
+    if (circle_b > 0)
+      length++;
+    if (circle_c > 0)
+      length++;
+  }
+};
+
+class MetaDataPopulate : public vtkm::worklet::WorkletMapField
+{
+public:
+  using ControlSignature =
+    void(FieldIn, FieldIn, FieldIn, FieldIn, FieldIn, WholeArrayOut, WholeArrayOut);
+
+  template <typename IdArray, typename DataArray>
+  void operator()(const vtkm::Id offset,
+                  const vtkm::FloatDefault& background,
+                  const vtkm::FloatDefault& circle_a,
+                  const vtkm::FloatDefault& circle_b,
+                  const vtkm::FloatDefault& circle_c,
+                  IdArray& matIds,
+                  DataArray& matVFs) const
+  {
+    vtkm::Id index = offset;
+    if (background > 0)
+    {
+      matIds.Set(index, 1);
+      matVFs.Set(index, background);
+      index++;
+    }
+    if (circle_a > 0)
+    {
+      matIds.Set(index, 2);
+      matVFs.Set(index, circle_a);
+      index++;
+    }
+    if (circle_b > 0)
+    {
+      matIds.Set(index, 3);
+      matVFs.Set(index, circle_b);
+      index++;
+    }
+    if (circle_c > 0)
+    {
+      matIds.Set(index, 4);
+      matVFs.Set(index, circle_c);
+      index++;
+    }
+  }
+};
+
+vtkm::cont::DataSet GetDataForFilter(vtkm::cont::DataSet& data)
+{
+  using IdArray = vtkm::cont::ArrayHandle<vtkm::Id>;
+  using DataArray = vtkm::cont::ArrayHandle<vtkm::FloatDefault>;
+
+  DataArray backArr;
+  data.GetField("mesh_topo/background").GetData().AsArrayHandle(backArr);
+  DataArray cirAArr;
+  data.GetField("mesh_topo/circle_a").GetData().AsArrayHandle(cirAArr);
+  DataArray cirBArr;
+  data.GetField("mesh_topo/circle_b").GetData().AsArrayHandle(cirBArr);
+  DataArray cirCArr;
+  data.GetField("mesh_topo/circle_c").GetData().AsArrayHandle(cirCArr);
+
+  IdArray length;
+  IdArray offset;
+  IdArray matIds;
+  DataArray matVFs;
+  vtkm::cont::Invoker invoker;
+
+  invoker(MetaDataLength{}, backArr, cirAArr, cirBArr, cirCArr, length);
+
+  vtkm::cont::Algorithm::ScanExclusive(length, offset);
+
+  vtkm::Id total = vtkm::cont::Algorithm::Reduce(length, 0);
+  matIds.Allocate(total);
+  matVFs.Allocate(total);
+
+  invoker(MetaDataPopulate{}, offset, backArr, cirAArr, cirBArr, cirCArr, matIds, matVFs);
+
+  data.AddField(vtkm::cont::Field("scatter_pos", vtkm::cont::Field::Association::CELL_SET, offset));
+  data.AddField(vtkm::cont::Field("scatter_len", vtkm::cont::Field::Association::CELL_SET, length));
+  data.AddField(
+    vtkm::cont::Field("scatter_ids", vtkm::cont::Field::Association::WHOLE_MESH, matIds));
+  data.AddField(
+    vtkm::cont::Field("scatter_vfs", vtkm::cont::Field::Association::WHOLE_MESH, matVFs));
+
+  return data;
+}
+
+void TestMIRVenn250()
+{
+  std::string vennFile = vtkm::cont::testing::Testing::DataPath("uniform/venn250.vtk");
+  vtkm::io::VTKDataSetReader reader(vennFile);
+  vtkm::cont::DataSet data = reader.ReadDataSet();
+  vtkm::cont::DataSet forMIR = GetDataForFilter(data);
+
+  vtkm::filter::MIRFilter mir;
+  mir.SetIDWholeSetName("scatter_ids");
+  mir.SetPositionCellSetName("scatter_pos");
+  mir.SetLengthCellSetName("scatter_len");
+  mir.SetVFWholeSetName("scatter_vfs");
+  mir.SetErrorScaling(vtkm::Float64(0.2));
+  mir.SetScalingDecay(vtkm::Float64(1.0));
+  mir.SetMaxIterations(vtkm::IdComponent(0)); // =0 -> No iterations..
+  // Only useful for iterations >= 1, will stop iterating if total % error for entire mesh is less than this value
+  // Note it is mathematically impossible to obtain 0% error outside of VERY special cases (neglecting float error)
+  mir.SetMaxPercentError(vtkm::Float64(0.00001));
+
+  std::cerr << "Input cells : " << forMIR.GetNumberOfCells() << std::endl;
+  std::cerr << "Input points : " << forMIR.GetNumberOfPoints() << std::endl;
+
+  vtkm::cont::Timer timer;
+  timer.Start();
+
+  vtkm::cont::DataSet fromMIR = mir.Execute(forMIR);
+
+  timer.Stop();
+
+  std::cerr << "Finished in : " << timer.GetElapsedTime() << std::endl;
+  std::cerr << "Output cells : " << fromMIR.GetNumberOfCells() << std::endl;
+  std::cerr << "Output points : " << fromMIR.GetNumberOfPoints() << std::endl;
+}
+
+void TestMIRSynthetic()
 {
   vtkm::cont::DataSet ds = GetTestDataSet();
 
@@ -116,6 +259,12 @@ void TestMIR()
 
   // Test if ds_from_mir has 40 cells
   VTKM_TEST_ASSERT(ds_from_mir.GetNumberOfCells() == 40, "Wrong number of output cells");
+}
+
+void TestMIR()
+{
+  TestMIRSynthetic();
+  TestMIRVenn250();
 }
 
 int UnitTestMIRFilter(int argc, char* argv[])
