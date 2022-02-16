d22a509b52510f56f0495d32bf74bdf01d7bd076
Kenneth Moreland <morelandkd@ornl.gov>
2022-02-16 07:24:04 -0700

    Split CoordinateSystemTransform.h
    This header file contained two filters: `CylindricalCoordinateTransform`
    and `SphericalCoordinateTransform`. By convention, each header (and
    source) file should contain one filter. Now that filters are split into
    separate libraries, there is little reason to combine things into header
    files.

--- vtkm/filter/field_transform/CylindricalCoordinateTransform.cxx
+++ vtkm/filter/field_transform/CylindricalCoordinateTransform.cxx
@@ -8,7 +8,7 @@
 //  PURPOSE.  See the above copyright notice for more information.
 //============================================================================
 
-#include <vtkm/filter/field_transform/CoordinateSystemTransform.h>
+#include <vtkm/filter/field_transform/CylindricalCoordinateTransform.h>
 #include <vtkm/filter/field_transform/worklet/CoordinateSystemTransform.h>
 
 namespace vtkm
@@ -17,7 +17,7 @@ namespace filter
 {
 namespace field_transform
 {
-//-----------------------------------------------------------------------------
+
 vtkm::cont::DataSet CylindricalCoordinateTransform::DoExecute(const vtkm::cont::DataSet& inDataSet)
 {
   vtkm::cont::UnknownArrayHandle outArray;
@@ -42,30 +42,6 @@ vtkm::cont::DataSet CylindricalCoordinateTransform::DoExecute(const vtkm::cont::
   return outDataSet;
 }
 
-//-----------------------------------------------------------------------------
-vtkm::cont::DataSet SphericalCoordinateTransform::DoExecute(const vtkm::cont::DataSet& inDataSet)
-{
-  vtkm::cont::UnknownArrayHandle outArray;
-
-  auto resolveType = [&](const auto& concrete) {
-    // use std::decay to remove const ref from the decltype of concrete.
-    using T = typename std::decay_t<decltype(concrete)>::ValueType;
-    vtkm::cont::ArrayHandle<T> result;
-    vtkm::worklet::SphericalCoordinateTransform worklet{ this->CartesianToSpherical };
-    worklet.Run(concrete, result);
-    outArray = result;
-  };
-  this->CastAndCallVecField<3>(this->GetFieldFromDataSet(inDataSet), resolveType);
-
-  vtkm::cont::DataSet outDataSet =
-    this->CreateResult(inDataSet,
-                       inDataSet.GetCellSet(),
-                       vtkm::cont::CoordinateSystem("coordinates", outArray),
-                       [](vtkm::cont::DataSet& out, const vtkm::cont::Field& fieldToPass) {
-                         out.AddField(fieldToPass);
-                       });
-  return outDataSet;
-}
 } // namespace field_transform
 } // namespace filter
 } // namespace vtkm
