c661e4afbab4b9ab1045ec500243705e1babf6b9
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2022-07-08 16:39:46 -0400

    CI: refactor cmake.sh file

--- .gitlab/ci/config/cmake.sh
+++ .gitlab/ci/config/cmake.sh
@@ -5,21 +5,20 @@ set -x
 
 version="${1:-3.21.1}"
 
-# We require CMake >= 3.13 in the CI to support CUDA builds
-readonly -A linuxParamsByVersion=(
-['3.13.5']='e2fd0080a6f0fc1ec84647acdcd8e0b4019770f48d83509e6a5b0b6ea27e5864	Linux'
-['3.21.1']='bf496ce869d0aa8c1f57e4d1a2e50c8f2fb12a6cd7ccb37ad743bb88f6b76a1e	linux'
-)
-
-if [ -z "${linuxParamsByVersion[$version]}" ]
-then
-  echo "Given version ($version) is unsupported"
-  exit 1
-fi
-
 case "$( uname -s )" in
     Linux)
         shatool="sha256sum"
+        # We require CMake >= 3.13 in the CI to support CUDA builds
+        readonly -A linuxParamsByVersion=(
+        ['3.13.5']='e2fd0080a6f0fc1ec84647acdcd8e0b4019770f48d83509e6a5b0b6ea27e5864	Linux'
+        ['3.21.1']='bf496ce869d0aa8c1f57e4d1a2e50c8f2fb12a6cd7ccb37ad743bb88f6b76a1e	linux'
+        )
+
+        if [ -z "${linuxParamsByVersion[$version]}" ]
+        then
+          echo "Given version ($version) is unsupported"
+          exit 1
+        fi
         sha256sum=$(cut -f 1 <<<"${linuxParamsByVersion[$version]}")
         platform=$(cut -f 2 <<<"${linuxParamsByVersion[$version]}")
         arch="x86_64"
