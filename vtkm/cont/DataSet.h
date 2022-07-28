Viskores/viskores

DataSet::GetFieldIndex should not throw
As per its documentation, DataSet::GetFieldIndex should return -1 when
a field with the given name is not found.

vtkm/cont/DataSet.h
@@ -50,25 +50,19 @@ public:
   bool HasField(const std::string& name,
                 vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::Any) const
   {
-    bool found = false;
-    this->FindFieldIndex(name, assoc, found);
-    return found;
+    return (this->GetFieldIndex(name, assoc) != -1);
   }
 
   VTKM_CONT
   bool HasCellField(const std::string& name) const
   {
-    bool found = false;
-    this->FindFieldIndex(name, vtkm::cont::Field::Association::Cells, found);
-    return found;
+    return (this->GetFieldIndex(name, vtkm::cont::Field::Association::Cells) != -1);
   }
 
   VTKM_CONT
   bool HasPointField(const std::string& name) const
   {
-    bool found = false;
-    this->FindFieldIndex(name, vtkm::cont::Field::Association::Points, found);
-    return found;
+    return (this->GetFieldIndex(name, vtkm::cont::Field::Association::Points) != -1);
   }
 
 
@@ -85,18 +79,12 @@ public:
   VTKM_CONT
   const vtkm::cont::Field& GetField(
     const std::string& name,
-    vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::Any) const
-  {
-    return this->GetField(this->GetFieldIndex(name, assoc));
-  }
+    vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::Any) const;
 
   VTKM_CONT
   vtkm::cont::Field& GetField(
     const std::string& name,
-    vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::Any)
-  {
-    return this->GetField(this->GetFieldIndex(name, assoc));
-  }
+    vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::Any);
   //@}
 
   /// Returns the first cell field that matches the provided name.
@@ -296,11 +284,6 @@ private:
   std::vector<vtkm::cont::CoordinateSystem> CoordSystems;
   std::map<FieldCompare::Key, vtkm::cont::Field, FieldCompare> Fields;
   vtkm::cont::UnknownCellSet CellSet;
-
-  VTKM_CONT
-  vtkm::Id FindFieldIndex(const std::string& name,
-                          vtkm::cont::Field::Association association,
-                          bool& found) const;
 };
 
 } // namespace cont

Kitware/VTK-m

vtkm/cont/DataSet.h
@@ -51,25 +51,19 @@ public:
   bool HasField(const std::string& name,
                 vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::ANY) const
   {
-    bool found = false;
-    this->FindFieldIndex(name, assoc, found);
-    return found;
+    return (this->GetFieldIndex(name, assoc) != -1);
   }
 
   VTKM_CONT
   bool HasCellField(const std::string& name) const
   {
-    bool found = false;
-    this->FindFieldIndex(name, vtkm::cont::Field::Association::CELL_SET, found);
-    return found;
+    return (this->GetFieldIndex(name, vtkm::cont::Field::Association::CELL_SET) != -1);
   }
 
   VTKM_CONT
   bool HasPointField(const std::string& name) const
   {
-    bool found = false;
-    this->FindFieldIndex(name, vtkm::cont::Field::Association::POINTS, found);
-    return found;
+    return (this->GetFieldIndex(name, vtkm::cont::Field::Association::POINTS) != -1);
   }
 
 
@@ -86,18 +80,12 @@ public:
   VTKM_CONT
   const vtkm::cont::Field& GetField(
     const std::string& name,
-    vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::ANY) const
-  {
-    return this->GetField(this->GetFieldIndex(name, assoc));
-  }
+    vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::ANY) const;
 
   VTKM_CONT
   vtkm::cont::Field& GetField(
     const std::string& name,
-    vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::ANY)
-  {
-    return this->GetField(this->GetFieldIndex(name, assoc));
-  }
+    vtkm::cont::Field::Association assoc = vtkm::cont::Field::Association::ANY);
   //@}
 
   /// Returns the first cell field that matches the provided name.
@@ -292,11 +280,6 @@ private:
   std::vector<vtkm::cont::CoordinateSystem> CoordSystems;
   std::map<FieldCompare::Key, vtkm::cont::Field, FieldCompare> Fields;
   vtkm::cont::DynamicCellSet CellSet;
-
-  VTKM_CONT
-  vtkm::Id FindFieldIndex(const std::string& name,
-                          vtkm::cont::Field::Association association,
-                          bool& found) const;
 };
 
 } // namespace cont