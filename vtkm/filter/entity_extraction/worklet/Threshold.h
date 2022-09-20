924b0019382ae03a3fb478c2b3b60e5949f0d046
Sujin Philip <sujin.philip@kitware.com>
2022-09-20 12:07:46 -0400

    Add more features to Threshold
    1. Now we can threshold for values below or above a given value, instead
       of just between two values.
    2. Supports multi-component fields instead of just scalar fields.
    3. The threshold criteria can be tested against just one specified
       component or against all of the components, and pass if any or all of
       the components pass.
    4. The results of the threshold operation can be inverted.

--- vtkm/filter/entity_extraction/worklet/Threshold.h
+++ vtkm/filter/entity_extraction/worklet/Threshold.h
@@ -17,10 +17,13 @@
 #include <vtkm/cont/Algorithm.h>
 #include <vtkm/cont/ArrayHandle.h>
 #include <vtkm/cont/ArrayHandleIndex.h>
+#include <vtkm/cont/ArrayHandleTransform.h>
 #include <vtkm/cont/CellSetPermutation.h>
 #include <vtkm/cont/Field.h>
 #include <vtkm/cont/UncertainCellSet.h>
 #include <vtkm/cont/UnknownCellSet.h>
+
+#include <vtkm/UnaryPredicates.h>
 
 namespace vtkm
 {
@@ -41,105 +44,186 @@ public:
     VTKM_CONT
     ThresholdByPointField()
       : Predicate()
-      , ReturnAllInRange()
+      , AllPointsMustPass()
     {
     }
 
     VTKM_CONT
-    explicit ThresholdByPointField(const UnaryPredicate& predicate, const bool& returnAllInRange)
+    explicit ThresholdByPointField(const UnaryPredicate& predicate, bool allPointsMustPass)
       : Predicate(predicate)
-      , ReturnAllInRange(returnAllInRange)
+      , AllPointsMustPass(allPointsMustPass)
     {
     }
 
     template <typename ScalarsVecType>
     VTKM_EXEC bool operator()(const ScalarsVecType& scalars, vtkm::Id count) const
     {
-      bool pass;
-      if (this->ReturnAllInRange)
-        pass = true;
-      else
-        pass = false;
-
+      bool pass = this->AllPointsMustPass ? true : false;
       for (vtkm::IdComponent i = 0; i < count; ++i)
       {
-        //Only pass a cell if it meets the validity requirement
-        //"all in range" or "part on range"
-        if (this->ReturnAllInRange)
+        if (this->AllPointsMustPass)
+        {
           pass &= this->Predicate(scalars[i]);
+        }
         else
+        {
           pass |= this->Predicate(scalars[i]);
+        }
       }
+
       return pass;
     }
 
   private:
     UnaryPredicate Predicate;
-    bool ReturnAllInRange;
+    bool AllPointsMustPass;
   };
 
-  template <typename CellSetType, typename ValueType, typename StorageType, typename UnaryPredicate>
-  vtkm::cont::CellSetPermutation<CellSetType> RunImpl(
-    const CellSetType& cellSet,
-    const vtkm::cont::ArrayHandle<ValueType, StorageType>& field,
-    const vtkm::cont::Field::Association fieldType,
-    const UnaryPredicate& predicate,
-    const bool returnAllInRange = false)
+  template <typename Operator>
+  class CombinePassFlagsWorklet : public vtkm::worklet::WorkletMapField
   {
-    using OutputType = vtkm::cont::CellSetPermutation<CellSetType>;
+  public:
+    using ControlSignature = void(FieldInOut, FieldIn);
+    using ExecitionSignature = void(_1, _2);
+
+    VTKM_CONT
+    explicit CombinePassFlagsWorklet(const Operator& combine)
+      : Combine(combine)
+    {
+    }
+
+    VTKM_EXEC void operator()(bool& combined, bool incoming) const
+    {
+      combined = this->Combine(combined, incoming);
+    }
+
+  private:
+    Operator Combine;
+  };
+
+  template <typename Operator>
+  void CombinePassFlags(const vtkm::cont::ArrayHandle<bool>& passFlagsIn, const Operator& combine)
+  {
+    if (this->PassFlags.GetNumberOfValues() == 0) // Is initialization needed?
+    {
+      this->PassFlags = passFlagsIn;
+    }
+    else
+    {
+      DispatcherMapField<CombinePassFlagsWorklet<Operator>> dispatcher(
+        CombinePassFlagsWorklet<Operator>{ combine });
+      dispatcher.Invoke(this->PassFlags, passFlagsIn);
+    }
+    this->PassFlagsModified = true;
+  }
+
+  // special no-op combine operator for combining `PassFlags` results of incremental runs
+  struct NoOp
+  {
+  };
+
+  void CombinePassFlags(const vtkm::cont::ArrayHandle<bool>& passFlagsIn, NoOp)
+  {
+    this->PassFlags = passFlagsIn;
+    this->PassFlagsModified = true;
+  }
+
+  /// Incrementally run the worklet on the given parameters. Each run should get the
+  /// same `cellSet`. An array of pass/fail flags is maintained internally. The `passFlagsCombine`
+  /// operator is used to combine the current result to the incremental results. Finally, use
+  /// `GenerateResultCellSet` to get the thresholded cellset.
+  template <typename ValueType,
+            typename StorageType,
+            typename UnaryPredicate,
+            typename PassFlagsCombineOp>
+  void RunIncremental(const vtkm::cont::UnknownCellSet& cellSet,
+                      const vtkm::cont::ArrayHandle<ValueType, StorageType>& field,
+                      vtkm::cont::Field::Association fieldType,
+                      const UnaryPredicate& predicate,
+                      bool allPointsMustPass, // only considered when field association is `Points`
+                      const PassFlagsCombineOp& passFlagsCombineOp)
+  {
+    vtkm::cont::ArrayHandle<bool> passFlags;
 
     switch (fieldType)
     {
       case vtkm::cont::Field::Association::Points:
       {
         using ThresholdWorklet = ThresholdByPointField<UnaryPredicate>;
-        vtkm::cont::ArrayHandle<bool> passFlags;
 
-        ThresholdWorklet worklet(predicate, returnAllInRange);
+        ThresholdWorklet worklet(predicate, allPointsMustPass);
         DispatcherMapTopology<ThresholdWorklet> dispatcher(worklet);
         dispatcher.Invoke(cellSet, field, passFlags);
-
-        vtkm::cont::Algorithm::CopyIf(vtkm::cont::ArrayHandleIndex(passFlags.GetNumberOfValues()),
-                                      passFlags,
-                                      this->ValidCellIds);
-
         break;
       }
       case vtkm::cont::Field::Association::Cells:
       {
-        vtkm::cont::Algorithm::CopyIf(vtkm::cont::ArrayHandleIndex(field.GetNumberOfValues()),
-                                      field,
-                                      this->ValidCellIds,
-                                      predicate);
+        vtkm::cont::Algorithm::Copy(vtkm::cont::make_ArrayHandleTransform(field, predicate),
+                                    passFlags);
         break;
       }
-
       default:
         throw vtkm::cont::ErrorBadValue("Expecting point or cell field.");
     }
 
-    return OutputType(this->ValidCellIds, cellSet);
+    this->CombinePassFlags(passFlags, passFlagsCombineOp);
   }
 
-  template <typename ValueType, typename StorageType, typename UnaryPredicate>
-  vtkm::cont::UnknownCellSet Run(const vtkm::cont::UnknownCellSet& cellSet,
-                                 const vtkm::cont::ArrayHandle<ValueType, StorageType>& field,
-                                 const vtkm::cont::Field::Association fieldType,
-                                 const UnaryPredicate& predicate,
-                                 const bool returnAllInRange = false)
+  vtkm::cont::ArrayHandle<vtkm::Id> GetValidCellIds() const
+  {
+    if (this->PassFlagsModified)
+    {
+      vtkm::cont::Algorithm::CopyIf(
+        vtkm::cont::ArrayHandleIndex(this->PassFlags.GetNumberOfValues()),
+        this->PassFlags,
+        this->ValidCellIds);
+      this->PassFlagsModified = false;
+    }
+    return this->ValidCellIds;
+  }
+
+  vtkm::cont::UnknownCellSet GenerateResultCellSet(const vtkm::cont::UnknownCellSet& cellSet)
   {
     vtkm::cont::UnknownCellSet output;
+
     CastAndCall(cellSet, [&](auto concrete) {
       output = vtkm::worklet::CellDeepCopy::Run(
-        this->RunImpl(concrete, field, fieldType, predicate, returnAllInRange));
+        vtkm::cont::make_CellSetPermutation(this->GetValidCellIds(), concrete));
     });
+
     return output;
   }
 
-  vtkm::cont::ArrayHandle<vtkm::Id> GetValidCellIds() const { return this->ValidCellIds; }
+  // Invert the results stored in this worklet's state
+  void InvertResults()
+  {
+    vtkm::cont::Algorithm::Copy(
+      vtkm::cont::make_ArrayHandleTransform(this->PassFlags, vtkm::LogicalNot{}), this->PassFlags);
+    this->PassFlagsModified = true;
+  }
+
+  template <typename ValueType, typename StorageType, typename UnaryPredicate>
+  vtkm::cont::UnknownCellSet Run(
+    const vtkm::cont::UnknownCellSet& cellSet,
+    const vtkm::cont::ArrayHandle<ValueType, StorageType>& field,
+    vtkm::cont::Field::Association fieldType,
+    const UnaryPredicate& predicate,
+    bool allPointsMustPass = false, // only considered when field association is `Points`
+    bool invert = false)
+  {
+    this->RunIncremental(cellSet, field, fieldType, predicate, allPointsMustPass, NoOp{});
+    if (invert)
+    {
+      this->InvertResults();
+    }
+    return this->GenerateResultCellSet(cellSet);
+  }
 
 private:
-  vtkm::cont::ArrayHandle<vtkm::Id> ValidCellIds;
+  vtkm::cont::ArrayHandle<bool> PassFlags;
+
+  mutable bool PassFlagsModified = true;
+  mutable vtkm::cont::ArrayHandle<vtkm::Id> ValidCellIds;
 };
 }
 } // namespace vtkm::worklet
