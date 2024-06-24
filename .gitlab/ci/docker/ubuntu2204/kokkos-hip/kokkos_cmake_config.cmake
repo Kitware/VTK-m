c6f0e369863da049248183eef3d581920307b50a
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2024-06-24 18:07:45 -0400

    ci: update kokkos hip to 6.1, ubuntu 2204

--- .gitlab/ci/docker/ubuntu2204/kokkos-hip/kokkos_cmake_config.cmake
+++ .gitlab/ci/docker/ubuntu2204/kokkos-hip/kokkos_cmake_config.cmake
@@ -9,13 +9,10 @@
 ##============================================================================
 
 set(CMAKE_BUILD_TYPE     "release"                  CACHE STRING "")
-set(CMAKE_INSTALL_PREFIX /opt/kokkos                CACHE PATH "")
 set(CMAKE_C_COMPILER     /opt/rocm/llvm/bin/clang   CACHE FILEPATH "")
 set(CMAKE_CXX_COMPILER   /opt/rocm/llvm/bin/clang++ CACHE FILEPATH "")
-set(CMAKE_CXX_STANDARD   "14"                       CACHE STRING "")
 set(CMAKE_POSITION_INDEPENDENT_CODE ON              CACHE BOOL "")
 
 set(Kokkos_ENABLE_SERIAL ON                         CACHE BOOL "")
-set(Kokkos_ARCH_VEGA900  ON                         CACHE BOOL "")
 set(Kokkos_ENABLE_HIP    ON                         CACHE BOOL "")
 set(Kokkos_ENABLE_HIP_RELOCATABLE_DEVICE_CODE OFF   CACHE BOOL "")
